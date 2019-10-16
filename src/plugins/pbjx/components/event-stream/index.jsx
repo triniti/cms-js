693ae205571ea56b95862aaa699ac17a2ed09908
bhsiao55 <bhsiao55@gmail.com>
2019-10-15 18:23:34 -0700

    re #964 add local state to event stream. move status message to event stream

--- src/plugins/pbjx/components/event-stream/index.jsx
+++ src/plugins/pbjx/components/event-stream/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-did-update-set-state */
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -7,47 +8,80 @@ import {
   CardHeader,
   ListGroup,
   ListGroupItem,
-  ListGroupItemText,
+  ListGroupItemText, StatusMessage,
 } from '@triniti/admin-ui-plugin/components';
 import startCase from 'lodash/startCase';
 import moment from 'moment';
 import Message from '@gdbots/pbj/Message';
+import Exception from '@gdbots/common/Exception';
+import { STATUS_FULFILLED, STATUS_NONE } from '@triniti/app/constants';
 import UserLink from './UserLink';
 import EventDetails from './EventDetails';
 import RawViewButton from '../raw-view-button';
-
 
 class EventStream extends React.Component {
   static propTypes = {
     events: PropTypes.arrayOf(PropTypes.instanceOf(Message)),
     getUser: PropTypes.func.isRequired,
-    response: PropTypes.instanceOf(Message).isRequired,
+    response: PropTypes.instanceOf(Message),
     onLoadMore: PropTypes.func.isRequired,
+    status: PropTypes.string,
+    exception: PropTypes.instanceOf(Exception),
   };
 
   static defaultProps = {
     events: [],
+    status: STATUS_NONE,
+    exception: null,
+    response: null,
   };
 
   constructor(props) {
     super(props);
-    console.log('construct');
+    this.state = {
+      allEvents: [],
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { events } = this.props;
+    const { allEvents } = this.state;
+
+    if (prevProps.events.length === 0 && events.length === 0) {
+      return;
+    }
+    if (prevProps.events.length === 0 && events.length > 0) {
+      this.setState({ allEvents: [...allEvents, ...events] });
+    }
+    if (prevProps.events.length > 0 && events.length > 0) {
+      // If new set of events are not passed in do not update state.
+      if (prevProps.events[0].get('event_id') !== events[0].get('event_id')) {
+        this.setState({ allEvents: [...allEvents, ...events] });
+      }
+    }
   }
 
   render() {
     const {
-      events,
       getUser,
       response,
       onLoadMore: handleLoadMore,
+      status,
+      exception,
     } = this.props;
+
+    const { allEvents } = this.state;
 
     return (
       <Card>
         <CardHeader>History</CardHeader>
         <CardBody indent>
           <ListGroup borderless>
-            {events.map((event) => {
+            {
+              (!response || status !== STATUS_FULFILLED)
+              && <StatusMessage status={status} exception={exception} />
+            }
+            {allEvents.map((event) => {
               const eventAction = startCase(event.generateMessageRef().getCurie().getMessage());
               const occurredAt = moment(event.get('occurred_at').toDate()).format('MMM DD, YYYY hh:mm:ss A');
               const occurredAtAgo = moment(event.get('occurred_at').toDate()).fromNow();
@@ -73,7 +107,7 @@ class EventStream extends React.Component {
             })}
           </ListGroup>
           {
-            response.get('has_more')
+            response && response.get('has_more')
             && <Button key="a" style={{ margin: 0 }} onClick={handleLoadMore} color="secondary">Load More</Button>
           }
         </CardBody>
--- src/plugins/pbjx/components/history/index.jsx
+++ src/plugins/pbjx/components/history/index.jsx
@@ -1,22 +1,13 @@
-/* eslint-disable react/no-did-update-set-state */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { STATUS_FULFILLED } from '@triniti/app/constants';
-import { Button, Card, CardBody, CardHeader, ListGroup, StatusMessage } from '@triniti/admin-ui-plugin/components';
 import createDelegateFactory from '@triniti/app/createDelegateFactory';
 import Message from '@gdbots/pbj/Message';
 import Schema from '@gdbots/pbj/Schema';
 import StreamId from '@gdbots/schemas/gdbots/pbjx/StreamId';
-import startCase from 'lodash/startCase';
-import moment from 'moment';
 import EventStream from '../event-stream';
 import selector from './selector';
 import delegateFactory from './delegate';
-import UserLink from '../event-stream/UserLink';
-import RawViewButton from '../raw-view-button';
-import EventDetails from '../event-stream/EventDetails';
-
 
 class History extends React.Component {
   static propTypes = {
@@ -40,37 +31,9 @@ class History extends React.Component {
     events: [],
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      allEvents: [],
-    };
-  }
-
   componentDidMount() {
     const { delegate, streamId, schema } = this.props;
     delegate.handleInitialize(streamId, schema);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { events } = this.props;
-    const { allEvents } = this.state;
-
-    if (prevProps.events.length === 0 && events.length === 0) {
-      console.log('BOTH EMPTY');
-      return;
-    }
-    if (prevProps.events.length === 0 && events.length > 0) {
-      console.log('push new state');
-      this.setState({ allEvents: [...allEvents, ...events] });
-    }
-    if (prevProps.events.length > 0 && events.length > 0) {
-      // Check to see if first event prop and prev event prop are the same.
-      // If same do not append to local event state
-      if (prevProps.events[0].get('event_id') !== events[0].get('event_id')) {
-        this.setState({ allEvents: [...allEvents, ...events] });
-      }
-    }
   }
 
   componentWillUnmount() {
@@ -90,30 +53,7 @@ class History extends React.Component {
       schema,
     } = this.props;
 
-    const { allEvents } = this.state;
-
-    if (!response || status !== STATUS_FULFILLED) {
-      return <StatusMessage status={status} exception={exception} />;
-    }
-    return (
-      <div>
-        {allEvents.map((event) => (
-          <p style={{ 'padding-bottom': '100px' }} key={event.get('event_id')}>{`${event.get('event_id')}`}</p>
-        ))}
-        {response.get('has_more')
-          && (
-            <Button
-              key="a"
-              style={{ margin: 0 }}
-              onClick={() => delegate.handleLoadMore(streamId, schema, response.get('last_occurred_at'))}
-              color="secondary"
-            >
-              Load More
-            </Button>
-          )}
-      </div>
-    );
-    // return <EventStream events={allEvents} getUser={getUser} response={response} onLoadMore={() => delegate.handleLoadMore(streamId, schema, response.get('last_occurred_at'))} />;
+    return <EventStream events={events} getUser={getUser} status={status} exception={exception} response={response} onLoadMore={() => delegate.handleLoadMore(streamId, schema, response.get('last_occurred_at'))} />;
   }
 }
 
