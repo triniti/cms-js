5fd722f6f1274f7ff48abe6d50e858dde44aa773
Greg Brown <greg@12dnetworks.com>
2019-11-04 10:43:11 -0800

    prep for v0.2.5

--- src/plugins/taxonomy/index.js
+++ src/plugins/taxonomy/index.js
@@ -12,7 +12,7 @@ import { serviceIds } from './constants';
 
 export default class TaxonomyPlugin extends Plugin {
   constructor() {
-    super('triniti', 'taxonomy', '0.2.4');
+    super('triniti', 'taxonomy', '0.2.5');
   }
 
   configure(app, bottle) {
