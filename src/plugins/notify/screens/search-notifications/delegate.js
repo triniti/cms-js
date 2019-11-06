646cce5f63bd43df91cc6d907c6267ee0ada1737
joel.capillo <joel.capillo@tmz.com>
2019-11-06 12:26:29 -0800

    include notification

--- src/plugins/notify/screens/search-notifications/delegate.js
+++ src/plugins/notify/screens/search-notifications/delegate.js
@@ -1,4 +1,6 @@
 import AbstractDelegate from '@triniti/cms/plugins/ncr/screens/search-nodes/AbstractDelegate';
+import SearchNotificationsSort from '@triniti/schemas/triniti/notify/enums/SearchNotificationsSort';
+
 import schemas from './schemas';
 
 class Delegate extends AbstractDelegate {
@@ -11,6 +13,16 @@ class Delegate extends AbstractDelegate {
   componentDidMount() {
     super.componentDidMount();
     this.dispatch(schemas.getAllApps.createMessage());
+  }
+
+  getSearchParams() {
+    const { searchNodesRequestState: { request }, statuses } = this.component.props;
+    const { CREATED_AT_DESC } = SearchNotificationsSort;
+    return {
+      app_ref: request ? request.get('app_ref') : null,
+      send_status: request ? request.get('send_status') : null,
+      sort: request ? request.get('sort', CREATED_AT_DESC).getValue() : CREATED_AT_DESC.getValue(),
+    };
   }
 }
 
