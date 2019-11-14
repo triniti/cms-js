801d4c79eb915c088d5eead2114288544cb77a2c
Eric Jacobson <contact@efjacobson.com>
2019-11-14 10:57:25 -0800

    prep for 0.2.10

--- src/plugins/raven/index.js
+++ src/plugins/raven/index.js
@@ -12,7 +12,7 @@ import Raven from './services/Raven';
 
 export default class RavenPlugin extends Plugin {
   constructor() {
-    super('triniti', 'raven', '0.2.8');
+    super('triniti', 'raven', '0.2.10');
   }
 
   configure(app, bottle) {
