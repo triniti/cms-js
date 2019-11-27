abd76de185fc2aaaf00e8b9665350075e7490de9
Eric Jacobson <contact@efjacobson.com>
2019-11-26 18:35:52 -0800

    refine start/stop flows

--- src/plugins/ovp/sagas/stopMediaLiveChannelFlow.js
+++ src/plugins/ovp/sagas/stopMediaLiveChannelFlow.js
@@ -1,13 +1,23 @@
 import { actionChannel, call, delay, fork, put, putResolve, race } from 'redux-saga/effects';
 import ChannelStopped from '@triniti/schemas/triniti/ovp.medialive/event/ChannelStoppedV1';
 import OperationTimedOut from '@triniti/cms/plugins/pbjx/exceptions/OperationTimedOut';
-import resolveSchema from '@triniti/cms/utils/resolveSchema';
 import sendAlert from '@triniti/admin-ui-plugin/actions/sendAlert';
-import stopMediaLiveChannel from '@triniti/cms/plugins/ovp/actions/stopMediaLiveChannel';
 import toast from '@triniti/admin-ui-plugin/utils/toast';
-import VideoV1Mixin from '@triniti/schemas/triniti/ovp/mixin/video/VideoV1Mixin';
-import waitForFlow from '@triniti/cms/plugins/ncr/sagas/waitForFlow';
 import waitForMyEvent from '@triniti/cms/plugins/ncr/sagas/waitForMyEvent';
+
+/**
+ * @param {Object} error
+ */
+function* failureFlow(error) {
+  const message = typeof error.getMessage === 'function' ? error.getMessage() : error.message;
+
+  yield call([toast, 'hide']);
+  yield put(sendAlert({
+    type: 'danger',
+    isDismissible: true,
+    message: `Stop MediaLive Channel Failed: ${message}`,
+  }));
+}
 
 export default function* stopMediaLiveChannelFlow({ pbj }) {
   try {
@@ -15,46 +25,24 @@ export default function* stopMediaLiveChannelFlow({ pbj }) {
     const eventChannel = yield actionChannel(ChannelStopped.schema().getCurie().toString());
     yield putResolve(pbj);
 
-    yield race({
+    const result = yield race({
       event: call(waitForMyEvent, eventChannel),
-      timeout: delay(1000),
+      timeout: delay(120000),
     });
 
-    const nodeRef = pbj.get('node_ref');
-    const wasSuccessful = yield call(
-      waitForFlow,
-      resolveSchema(VideoV1Mixin, 'request', 'get-video-request'),
-      nodeRef,
-      (response) => {
-        const isValid = response.pbj.isInSet('metas', 'medialive_channel_status'); // && response.pbj.get('metas').medialive_channel_status === 'RUNNING';
-        // todo: make it good like once the server is itself good like
-        return isValid;
-      },
-    );
-
-    yield call([toast, 'hide']);
-
-    if (wasSuccessful) {
-      yield put(stopMediaLiveChannel(nodeRef));
-      yield put(sendAlert({
-        type: 'success',
-        isDismissible: true,
-        delay: 3000,
-        message: 'Success! The MediaLive Channel was stopped.',
-      }));
-    } else {
-      yield put(sendAlert({
-        type: 'danger',
-        isDismissible: true,
-        message: `Stop MediaLive Channel Failed: ${(new OperationTimedOut(pbj)).getMessage()}`,
-      }));
+    if (result.timeout) {
+      yield call(failureFlow, new OperationTimedOut(pbj));
+      return;
     }
-  } catch (e) {
+
     yield call([toast, 'hide']);
     yield put(sendAlert({
-      type: 'danger',
+      type: 'success',
       isDismissible: true,
-      message: `Stop MediaLive Channel Failed: ${e.message}.`,
+      delay: 3000,
+      message: 'Success! The MediaLive Channel was stopped.',
     }));
+  } catch (e) {
+    yield call(failureFlow, e);
   }
 }
