c168c691d0412397b5c02817edf9186ac2b76e08
Eric Jacobson <contact@efjacobson.com>
2019-12-13 18:01:11 -0800

    add class name to user picker field

--- src/plugins/iam/components/user-picker-field/index.jsx
+++ src/plugins/iam/components/user-picker-field/index.jsx
@@ -1,16 +1,18 @@
 import { FormGroup, Label } from '@triniti/admin-ui-plugin/components';
+import kebabCase from 'lodash/kebabCase';
 import NodePickerField from '@triniti/cms/plugins/ncr/components/node-picker-field';
 import PropTypes from 'prop-types';
 import React from 'react';
 import constants from './constants';
 import schemas from './schemas';
 
-const UserPickerField = ({ label, isEditMode, placeholder, ...rest }) => (
-  <FormGroup>
+const UserPickerField = ({ fields, label, isEditMode, placeholder, ...rest }) => (
+  <FormGroup className={`user-picker-field user-picker-field-${kebabCase(fields.name)}`}>
     <Label>{label}</Label>
     <NodePickerField
       {...rest}
       constants={constants}
+      fields={fields}
       isClearable
       isDisabled={!isEditMode}
       isMulti={false}
@@ -21,6 +23,7 @@ const UserPickerField = ({ label, isEditMode, placeholder, ...rest }) => (
 );
 
 UserPickerField.propTypes = {
+  fields: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   isEditMode: PropTypes.bool.isRequired,
   label: PropTypes.string,
   placeholder: PropTypes.string,
