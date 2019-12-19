4faebfb6dd6191c08b8aeee4b75758e7e08a2cdc
Javier Lopez <javier.lopez@tmz.com>
2019-12-19 12:05:15 -0800

    Added TikTok Embed Block

---
+++ src/plugins/blocksmith/components/tiktok-embed-block-modal/getTikTokId.test.js
@@ -0,0 +1,27 @@
+import test from 'tape';
+import getTikTokId from './getTikTokId';
+
+test('TikTokBlockEmbed', (t) => {
+  const url = '6768820529194372357';
+  const actual = getTikTokId(url);
+  const expected = '6768820529194372357';
+  t.deepEqual(actual, expected, 'it should accept a TikTok Id and return: 6768820529194372357');
+  t.end();
+});
+
+test('TikTokBlockEmbed', (t) => {
+  const url = 'https://www.tiktok.com/embed/6768820529194372357';
+  const actual = getTikTokId(url);
+  const expected = '6768820529194372357';
+  t.deepEqual(actual, expected, 'it should accept a TikTok Embed URL and return: 6768820529194372357');
+  t.end();
+});
+
+
+test('TikTokBlockEmbed', (t) => {
+  const url = '<iframe width="340" height="700" src="https://www.tiktok.com/embed/6768820529194372357" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  const actual = getTikTokId(url);
+  const expected = '6768820529194372357';
+  t.deepEqual(actual, expected, 'it should accept a TikTok Embed and return: 6768820529194372357');
+  t.end();
+});
