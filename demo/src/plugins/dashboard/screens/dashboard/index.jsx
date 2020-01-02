c2c0520e2e89f6861ee7da8bb1a60835449ac99d
Nanette Julius <nanette.codes@gmail.com>
2020-01-02 11:29:03 -0800

    1097 active edits tab new (#147)
    * initial commit
    
    * fixed refresh bug on news tab
    
    * changes
    
    * more changes
    
    * updated raven constants
    
    * morer changes
    
    * removed unneeded returns
    
    * removed unneeded constants updated thunk to reduce
    
    * fixed nav dashboard route
    
    * updates to use one table
    
    * updated table

--- demo/src/plugins/dashboard/screens/dashboard/index.jsx
+++ demo/src/plugins/dashboard/screens/dashboard/index.jsx
@@ -1,39 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ActiveEditsTable from '@triniti/cms/plugins/raven/components/active-edits-table';
 import NodeStatus from '@gdbots/schemas/gdbots/ncr/enums/NodeStatus';
 import { Col, Container, Row, Screen } from '@triniti/admin-ui-plugin/components';
 import TopArticles from '../../components/top-articles';
 
-const Dashboard = ({ location }) => (
+const Dashboard = ({ location, match: { params: { tab } } }) => (
   <Screen
-    header="Dashboard"
+    tabs={[
+      {
+        to: 'news',
+        text: 'news',
+      },
+      {
+        to: 'active-edits',
+        text: 'active edits',
+      },
+    ]}
     maxWidth="100%"
     title="Dashboard"
+    // NOTE: temporary fix until admin-ui solution for no breadcrumbs
+    breadcrumbs={{ length: null }}
     body={(
       <Container fluid className="dashboard">
-        <Row>
-          <Col lg="6">
-            <TopArticles
-              location={location}
-              title="Homepage Published"
-              search={{ count: 40, status: NodeStatus.PUBLISHED, slotting_key: 'home' }}
-            />
-          </Col>
-          <Col lg="6">
-            <TopArticles
-              location={location}
-              title="Homepage Draft"
-              search={{ count: 10, status: NodeStatus.DRAFT, q: '+is_homepage_news:true' }}
-            />
-          </Col>
-        </Row>
+        { tab === 'news'
+             && (
+             <Row>
+               <Col lg="6">
+                 <TopArticles
+                   location={location}
+                   title="Homepage Published"
+                   search={{ count: 40, status: NodeStatus.PUBLISHED, slotting_key: 'home' }}
+                 />
+               </Col>
+               <Col lg="6">
+                 <TopArticles
+                   location={location}
+                   title="Homepage Draft"
+                   search={{ count: 10, status: NodeStatus.DRAFT, q: '+is_homepage_news:true' }}
+                 />
+               </Col>
+             </Row>
+             )}
+        {tab === 'active-edits'
+             && (
+             <Row>
+               <Col lg="12">
+                 <ActiveEditsTable />
+               </Col>
+             </Row>
+             )}
       </Container>
-    )}
+      )}
   />
 );
-
 Dashboard.propTypes = {
   location: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
 export default Dashboard;
