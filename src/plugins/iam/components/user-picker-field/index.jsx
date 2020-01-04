1c254afb7d26b33fd150a83541b82b43947c997c
efjacobson <efjacobson@users.noreply.github.com>
2020-01-03 18:20:08 -0800

    User picker for author_ref (#146)
    * create branch
    
    * stub user picker
    
    * working user picker for author_ref
    
    * changelog
    
    * add class name to user picker field
    
    * changelog

--- src/plugins/iam/components/user-picker-field/index.jsx
+++ src/plugins/iam/components/user-picker-field/index.jsx
@@ -1,16 +1,18 @@
 import { FormGroup, Label } from '@triniti/admin-ui-plugin/components';
+import kebabCase from 'lodash/kebabCase';
 import NodePickerField from '@triniti/cms/plugins/ncr/components/node-picker-field';
 import PropTypes from 'prop-types';
 import React from 'react';
 import constants from './constants';
 import schemas from './schemas';
 
-const SponsorPickerField = ({ label, isEditMode, placeholder, ...rest }) => (
-  <FormGroup>
+const UserPickerField = ({ fields, label, isEditMode, placeholder, ...rest }) => (
+  <FormGroup className={`user-picker-field user-picker-field-${kebabCase(fields.name)}`}>
     <Label>{label}</Label>
     <NodePickerField
       {...rest}
       constants={constants}
+      fields={fields}
       isClearable
       isDisabled={!isEditMode}
       isMulti={false}
@@ -20,15 +22,16 @@ const SponsorPickerField = ({ label, isEditMode, placeholder, ...rest }) => (
   </FormGroup>
 );
 
-SponsorPickerField.propTypes = {
+UserPickerField.propTypes = {
+  fields: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   isEditMode: PropTypes.bool.isRequired,
   label: PropTypes.string,
   placeholder: PropTypes.string,
 };
 
-SponsorPickerField.defaultProps = {
-  label: 'Sponsor',
-  placeholder: 'Select a sponsor...',
+UserPickerField.defaultProps = {
+  label: 'User',
+  placeholder: 'Select a user...',
 };
 
-export default SponsorPickerField;
+export default UserPickerField;
