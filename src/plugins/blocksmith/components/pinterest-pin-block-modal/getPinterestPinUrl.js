6662e1d4c6c6f45fdcafd10b11613fd4414b6643
Javier Lopez <javier.lopez@tmz.com>
2020-01-07 16:00:39 -0800

    Updated getPinterestPinUrl.js

--- src/plugins/blocksmith/components/pinterest-pin-block-modal/getPinterestPinUrl.js
+++ src/plugins/blocksmith/components/pinterest-pin-block-modal/getPinterestPinUrl.js
@@ -1,9 +1,12 @@
-const getPinterestPinUrl = (pin) => {
-  const PINTEREST_PIN_URL_REGEX = /http?s:\/\/www\.pinterest\.com\/pin\/\w+/g;
-  const PINTEREST_PIN_ID_REGEX = /^[0-9]*$/g;
+const PINTEREST_PIN_URL_REGEX = /http?s:\/\/(www\.)?pinterest\.com\/pin\/\w+/g;
+const PINTEREST_PIN_ID_REGEX = /^[0-9]*$/g;
 
-  const isValidPinUrl = PINTEREST_PIN_URL_REGEX.test(pin);
-  const isValidPinId = PINTEREST_PIN_ID_REGEX.test(pin);
+let isValidPinUrl = false;
+let isValidPinId = false;
+
+const getPinterestPinUrl = (pin) => {
+  isValidPinUrl = PINTEREST_PIN_URL_REGEX.test(pin);
+  isValidPinId = PINTEREST_PIN_ID_REGEX.test(pin);
 
   if (isValidPinUrl) {
     return pin.match(PINTEREST_PIN_URL_REGEX)[0];
