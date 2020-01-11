9d36cd1787e46d5e4d055dab2f660dc6592a5e85
cohencodes <alon@cohencodes.com>
2020-01-10 17:53:08 -0800

    fix regex...

--- src/plugins/blocksmith/components/spotify-embed-block-modal/getSpotifyMediaId.js
+++ src/plugins/blocksmith/components/spotify-embed-block-modal/getSpotifyMediaId.js
@@ -1,16 +1,16 @@
-export const SPOTIFY_STRIP_MEDIA_ID_REGEX = /([0-9])\w+/g;
-export const SPOTIFY_STRIP_TYPE_REGEX = /(show|episode|artist|album|track|playlist)+/g;
-export const SPOTIFY_TRACK_REGEX = /spotify:track:[a-zA-Z0-9-]+/g;
-export const SPOTIFY_URL_REGEX = /https?:\/\/open\.spotify\.com\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/g;
-export const SPOTIFY_IFRAME_REGEX = /https?:\/\/open\.spotify\.com\/(embed|embed-podcast)\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/g;
+export const SPOTIFY_STRIP_MEDIA_ID_REGEX = /([0-9])\w+/;
+export const SPOTIFY_STRIP_TYPE_REGEX = /(show|episode|artist|album|track|playlist)+/;
+export const SPOTIFY_TRACK_REGEX = /spotify:track:[a-zA-Z0-9-]+/;
+export const SPOTIFY_URL_REGEX = /https?:\/\/open\.spotify\.com\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/;
+export const SPOTIFY_IFRAME_REGEX = /https?:\/\/open\.spotify\.com\/(embed|embed-podcast)\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/;
 
 // https://open.spotify.com/track/2rPw8mIZPu987REVeDbrjt
 
 const getSpotifyMediaId = (str) => {
   if (
-    /spotify:track:[a-zA-Z0-9-]+/g.test(str)
-    || /https?:\/\/open\.spotify\.com\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/g.test(str)
-    || /https?:\/\/open\.spotify\.com\/(embed|embed-podcast)\/(show|episode|artist|album|track|playlist)\/[a-zA-Z0-9-]+/g.test(str)) {
+    SPOTIFY_TRACK_REGEX.test(str)
+    || SPOTIFY_URL_REGEX.test(str)
+    || SPOTIFY_IFRAME_REGEX.test(str)) {
     if (str.match(SPOTIFY_STRIP_MEDIA_ID_REGEX).length) {
       const id = str.match(SPOTIFY_STRIP_MEDIA_ID_REGEX)[0];
       const type = str.match(SPOTIFY_STRIP_TYPE_REGEX)[0];
