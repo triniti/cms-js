6ec08bbe8505d513ee35c73b0ea1b0b3b43fd8b6
Joel Capillo <hunyoboy@gmail.com>
2020-01-10 16:29:22 -0800

    initial commit (#167)

--- src/plugins/blocksmith/components/draggable-text-block/index.jsx
+++ src/plugins/blocksmith/components/draggable-text-block/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { ContentBlock, EditorBlock } from 'draft-js';
@@ -7,41 +7,51 @@ import { handleDragEnd, handleDragStart } from '../../utils';
 import selector from './selector';
 
 // todo: incorporate the block buttons into this component and rename to TextBlockWrapper
-export const DraggableTextBlock = ({ block, draggable, handleFocus, offsetKey, ...rest }) => (
-  <>
-    {draggable && (
-    <div
-      className="drag-area draggable-top"
-      contentEditable={false}
-      draggable
-      onDragEnd={handleDragEnd}
-      onDragStart={handleDragStart(block.getKey())}
-    />
-    )}
-    <span
-      data-offset-key={offsetKey}
-      style={{ cursor: 'text' }}
-      role="presentation"
-      onClick={handleFocus}
-      onKeyPress={handleFocus}
-    >
-      <EditorBlock
-        block={block}
-        offsetKey={offsetKey}
-        {...rest}
+export const DraggableTextBlock = ({ block, draggable, handleFocus, offsetKey, ...rest }) => {
+  const [hovered, setHovered] = useState(false);
+  const onMouseEnter = (e) => {
+    e.preventDefault();
+    if (hovered) {
+      return;
+    }
+    handleFocus();
+    setHovered(true);
+  };
+  return (
+    <>
+      {draggable && (
+      <div
+        className="drag-area draggable-top"
+        contentEditable={false}
+        draggable
+        onDragEnd={handleDragEnd}
+        onDragStart={handleDragStart(block.getKey())}
       />
-    </span>
-    {draggable && (
-    <div
-      className="drag-area draggable-bottom"
-      contentEditable={false}
-      draggable
-      onDragEnd={handleDragEnd}
-      onDragStart={handleDragStart(block.getKey())}
-    />
-    )}
-  </>
-);
+      )}
+      <span
+        data-offset-key={offsetKey}
+        style={{ cursor: 'text' }}
+        role="presentation"
+        onMouseEnter={onMouseEnter}
+      >
+        <EditorBlock
+          block={block}
+          offsetKey={offsetKey}
+          {...rest}
+        />
+      </span>
+      {draggable && (
+      <div
+        className="drag-area draggable-bottom"
+        contentEditable={false}
+        draggable
+        onDragEnd={handleDragEnd}
+        onDragStart={handleDragStart(block.getKey())}
+      />
+      )}
+    </>
+  );
+};
 
 DraggableTextBlock.propTypes = {
   block: PropTypes.instanceOf(ContentBlock).isRequired,
