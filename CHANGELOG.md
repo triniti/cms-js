67c4b4e7203c71b36e5f2b9117922741e1d83e10
joel.capillo <joel.capillo@tmz.com>
2020-01-15 11:26:29 -0800

    initial commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 ## vN.N.N
 * Editing a slug during creation modal will result in a blank slug when hitting ENTER
 * Add More Fields On Designated Screens (display_title, credit_url, cta_text, cta_url, etc.)
+* Unselected images from Batch Edit once User clicks update.
 
 
 ## v0.3.3
@@ -11,7 +12,7 @@
 
 
 ## v0.3.2
-* Add Spotify URI to text area of spotify embed block to make it more clear which content type is being used. 
+* Add Spotify URI to text area of spotify embed block to make it more clear which content type is being used.
 * Spotify Embed Block requires pasting of embeds twice.
 * YouTube Playlist block: If user manually types anything into input field for EMBED CODE, URL, OR ID the CMS will crash
 * Text Editor toolbar does not appear after selecting option, leaving tab, and returning
--- src/plugins/curator/components/gallery-media/selector.js
+++ src/plugins/curator/components/gallery-media/selector.js
@@ -1,5 +1,7 @@
+import getCommand from '@triniti/cms/plugins/pbjx/selectors/getCommand';
 import getRequest from '@triniti/cms/plugins/pbjx/selectors/getRequest';
 import isGranted from '@triniti/cms/plugins/iam/selectors/isGranted';
+import PatchAssetsV1Mixin from '@triniti/schemas/triniti/dam/mixin/patch-assets/PatchAssetsV1Mixin';
 import schemas from './schemas';
 import { pbjxChannelNames } from '../../constants';
 
@@ -16,12 +18,14 @@ export default (state) => {
     pbjxChannelNames.GALLERY_MEDIA_SEARCH,
   );
 
+  const patchAssetsCommandState = getCommand(state, PatchAssetsV1Mixin.findOne().getCurie());
   const { response } = searchNodesRequestState;
   const isReorderGranted = isGranted(state, `${schemas.reorderGalleryAssets.getCurie()}`);
 
   return {
     nodes: response ? response.get('nodes', []) : [],
     isReorderGranted,
+    patchAssetsCommandState,
     searchNodesRequestState,
   };
 };
