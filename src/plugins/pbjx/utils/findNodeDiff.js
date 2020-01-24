3a3e86940640d50bf8993c073c3d9c525f81402b
bhsiao55 <bhsiao55@gmail.com>
2020-01-23 16:19:45 -0800

    re #1083 add missing keys that were removed so they can be shown to user

--- src/plugins/pbjx/utils/findNodeDiff.js
+++ src/plugins/pbjx/utils/findNodeDiff.js
@@ -1,11 +1,23 @@
 /* eslint-disable no-param-reassign */
-import { transform, isEqual, isObject } from 'lodash';
+import { transform, isEqual, isObject, isArray } from 'lodash';
 
 export const difference = (object, base) => transform(object, (result, value, key) => {
   // If object has etag then just compare etag
   if (isObject(value) && isObject(base[key]) && (value.etag !== base[key].etag)) {
     result[key] = value;
-  } else if (!isEqual(value, base[key])) {
+    return;
+  }
+  // If object is not blocks find any missing properties that were removed
+  if (isObject(value) && isObject(base[key]) && key !== 'blocks') {
+    const oldNodeKeys = Object.keys(base[key]);
+    const newNodeKeys = Object.keys(value);
+    const missingKeys = oldNodeKeys.filter((x) => !newNodeKeys.includes(x));
+
+    missingKeys.forEach((missingKey) => {
+      value[missingKey] = '[removed]';
+    });
+  }
+  if (!isEqual(value, base[key])) {
     result[key] = isObject(value) && isObject(base[key]) ? difference(value, base[key]) : value;
   }
 });
