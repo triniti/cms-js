0036f3c523c92f08c72e6f47fb901467c78ecc7a
Alon Cohen <alon@cohencodes.com>
2020-02-18 17:23:39 -0800

    Hotfix (#202)
    * change focus to button when hitting enter (#196)
    
    * Twitter block not accepting certain embed codes/links (#195)
    
    * Twitter block not accepting certain embed codes/links
    
    * add special characters to test embed
    
    * prepped for release v0.4.3
    
    Co-authored-by: efjacobson <efjacobson@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@
 * Cursor goes to top of content blocks or top of last block when using keyboard arrows to move to the end of the content blocks
 
 
+## v0.4.3
+* Twitter block not accepting certain embed codes/links
+* User Cannot press enter when creating nodes after entering title, without tabbing over to auto generated slug first.
+
+
 ## v0.4.2
 * Slug changes are overwritten when pressing enter in create node modals.
 
---
+++ src/plugins/blocksmith/components/twitter-tweet-block-modal/getTwitterTweetFields.js
@@ -0,0 +1,41 @@
+const HREF_ATTR_MATCH = /href=".+?"/;
+const LINK_TAG_MATCH = /<a.+?\/a>/g;
+const TWITTER_EMBED_REGEX = /^<blockquote.+https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9\\.]+(\n|.)+<\/script>$/m;
+const TWITTER_TWEET_REGEX = /https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9\\.]+/;
+const URL_MATCH = /(?:(?:^href=")|(?:"$))/g;
+
+export default (embedCode) => {
+  if (!TWITTER_EMBED_REGEX.test(embedCode)) {
+    return {
+      tweetText: null,
+      screenName: null,
+      tweetId: null,
+    };
+  }
+
+  let decodedEmbedCode = embedCode;
+  const linkMatches = embedCode.match(LINK_TAG_MATCH);
+  if (linkMatches) {
+    linkMatches.forEach((match) => {
+      const href = match.match(HREF_ATTR_MATCH);
+      if (href) {
+        const url = href[0].replace(URL_MATCH, '');
+        decodedEmbedCode = decodedEmbedCode.replace(url, decodeURIComponent(url));
+      }
+    });
+  }
+
+  const tweetText = decodedEmbedCode.replace(/<script.+<\/script>/, '')
+    .replace(/<blockquote.+?>/, '')
+    .replace('</blockquote>', '')
+    .replace(/(?:\n|(?:\s$))/g, '');
+  decodedEmbedCode = decodedEmbedCode.match(TWITTER_TWEET_REGEX)[0];
+  const screenName = decodedEmbedCode.split('/')[3];
+  const tweetId = decodedEmbedCode.split('/')[5];
+
+  return {
+    tweetText,
+    screenName,
+    tweetId,
+  };
+};
