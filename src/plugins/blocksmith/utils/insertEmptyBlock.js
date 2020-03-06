5cf5c6e7f2a00d173f74966caaff16ad3b17ffd5
Eric Jacobson <contact@efjacobson.com>
2020-03-06 11:48:11 -0800

    make parameters and docblock more gooder

--- src/plugins/blocksmith/utils/insertEmptyBlock.js
+++ src/plugins/blocksmith/utils/insertEmptyBlock.js
@@ -1,8 +1,4 @@
-import {
-  ContentBlock,
-  ContentState,
-  genKey,
-} from 'draft-js';
+import { ContentBlock, ContentState, genKey } from 'draft-js';
 import { List } from 'immutable';
 import { blockTypes } from '../constants';
 import constants from '../components/blocksmith/constants';
@@ -11,17 +7,19 @@ import getListBlocks from './getListBlocks';
 import isBlockAList from './isBlockAList';
 import normalizeKey from './normalizeKey';
 
+/* eslint-disable max-len */
 /**
  * Given a block's key, inserts an empty block before or after that block.
  *
- * @param {ContentState} contentState - a state instance of a DraftJs Editor
- * @param {string}       key          - a DraftJs block key
- * @param {Symbol}       position     - POSITION_BEFORE or POSITION_AFTER
- * @param {?string}      newBlockKey  - a new DraftJs block key to use for the new block
+ * @param {ContentState}                 contentState          - a ContentState instance of a DraftJs Editor
+ * @param {(ContentBlock|number|string)} insertionPointBlockId - a block, a block index, or a block key
+ * @param {Symbol}                       position              - POSITION_BEFORE or POSITION_AFTER
+ * @param {?string}                      newBlockKey           - a block key to use for the new block
  *
  * @returns {ContentState} a ContentState instance
  */
-export default (contentState, key, position, newBlockKey = genKey()) => {
+/* eslint-enable max-len */
+export default (contentState, insertionPointBlockId, position, newBlockKey = genKey()) => {
   if (position !== constants.POSITION_BEFORE && position !== constants.POSITION_AFTER) {
     throw new Error('You must provide a valid insertion position.');
   }
@@ -35,13 +33,13 @@ export default (contentState, key, position, newBlockKey = genKey()) => {
   if (!contentState.getBlocksAsArray().length) {
     newBlocksAsArray.push(emptyBlock);
   } else {
-    let block = findBlock(contentState, key);
-    if (isBlockAList(block)) {
-      const [finalListBlockNode] = getListBlocks(contentState, block).slice(-1);
-      block = findBlock(contentState, finalListBlockNode.getKey());
+    let insertionPointBlock = findBlock(contentState, insertionPointBlockId);
+    if (isBlockAList(insertionPointBlock)) {
+      const [finalListBlockNode] = getListBlocks(contentState, insertionPointBlock).slice(-1);
+      insertionPointBlock = findBlock(contentState, finalListBlockNode.getKey());
     }
     const blocksAsArray = contentState.getBlocksAsArray();
-    const blockIndex = blocksAsArray.indexOf(block);
+    const blockIndex = blocksAsArray.indexOf(insertionPointBlock);
     const offset = position === constants.POSITION_AFTER ? 1 : 0;
     newBlocksAsArray = blocksAsArray
       .slice(0, blockIndex + offset)
