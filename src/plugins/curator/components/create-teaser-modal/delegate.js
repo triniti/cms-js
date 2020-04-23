004ca7bc6c2fab6bd5dbb12c398d51d78c5427d3
Richard Sumilang <me@richardsumilang.com>
2020-04-22 23:52:42 -0700

    re: #218 apply image ref to asset teaser

--- src/plugins/curator/components/create-teaser-modal/delegate.js
+++ src/plugins/curator/components/create-teaser-modal/delegate.js
@@ -9,6 +9,15 @@ class Delegate extends AbstractDelegate {
       formName: formNames.CREATE_TEASER,
     }, dependencies);
   }
+
+  handleSubmit(data, formDispatch, formProps) {
+    // Copy image to primary image when an asset teaser is created.
+    if (data.type.value === 'asset-teaser') {
+      data.imageRef = data.targetRef;
+    }
+
+    return super.handleSubmit(data, formDispatch, formProps);
+  }
 }
 
 export { Delegate }; // to allow for site level customization
