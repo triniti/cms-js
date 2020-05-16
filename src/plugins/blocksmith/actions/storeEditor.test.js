feb12befbdf15104b40466ea68c6d68846f15dcd
Richard Sumilang <me@richardsumilang.com>
2020-05-15 17:51:45 -0700

    Fixing tests

--- src/plugins/blocksmith/actions/storeEditor.test.js
+++ src/plugins/blocksmith/actions/storeEditor.test.js
@@ -6,12 +6,14 @@ import storeEditor from './storeEditor';
 test('Blocksmith:action:storeEditor', (t) => {
   const formName = 'cryptonomicon';
   const editorState = EditorState.createEmpty();
+  const isDirty = true;
 
-  const actual = storeEditor(formName, editorState);
+  const actual = storeEditor(formName, editorState, isDirty);
   const expected = {
     type: actionTypes.EDITOR_STORED,
     formName,
     editorState,
+    isDirty,
   };
 
   t.same(actual, expected, 'it should create an EDITOR_STORED action with a formName.');
