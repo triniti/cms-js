98fe04474549ec68e8472ca2b45c940fbfff1bf9
Eric Jacobson <contact@efjacobson.com>
2020-07-30 19:25:51 -0700

    transcodeable/transcribable cards

---
+++ src/plugins/ovp/components/transcodeable-card/index.jsx
@@ -0,0 +1,72 @@
+import { Button, Card, Collapse, CardBody, CardHeader } from '@triniti/admin-ui-plugin/components';
+import artifactUrl from '@triniti/cms/plugins/ovp/utils/artifactUrl';
+import AssetId from '@triniti/schemas/triniti/dam/AssetId';
+import camelCase from 'lodash/camelCase';
+import damUrl from '@triniti/cms/plugins/dam/utils/damUrl';
+import Message from '@gdbots/pbj/Message';
+import NodeRef from '@gdbots/schemas/gdbots/ncr/NodeRef';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import startCase from 'lodash/startCase';
+
+import './styles.scss';
+
+const TranscodeableCard = ({ node }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const status = node.has('transcoding_status') ? node.get('transcoding_status').getValue() : 'unknown';
+  const videoId = AssetId.fromString(NodeRef.fromNode(node).getId());
+  const imageId = AssetId.fromString(`image_jpg_${videoId.getDate()}_${videoId.getUuid()}`);
+
+  return (
+    <Card key={node.get('_id')} className="transcodeable-card">
+      <CardHeader toggler className="pr-2">
+        <Button color="toggler" onClick={() => setIsOpen(!isOpen)} active={isOpen}>Transcoding</Button>
+        <div>
+          <small className="text-uppercase status-copy mr-0 pr-0">
+            status:
+          </small>
+          <small className={`text-uppercase status-copy mr-2 status-${status}`}>
+            {status}
+          </small>
+        </div>
+      </CardHeader>
+      <Collapse isOpen={isOpen}>
+        <CardBody className="pb-3">
+          {status !== 'completed'
+            ? <p>{`No artifacts available. Transcoding status: ${status}.`}</p>
+            : (
+              <ul className="pl-4">
+                {['original', 'manifest', 'video', 'tooltip-thumbnail-sprite', 'tooltip-thumbnail-track'].map((type) => (
+                  <li className="mb-1">{`${startCase(camelCase(type))}:`}
+                    <a
+                      href={artifactUrl(node, type)}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {` ${artifactUrl(node, type)}`}
+                    </a>
+                  </li>
+                ))}
+                <li className="mb-1">Image:
+                  <a
+                    href={damUrl(imageId)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {` ${damUrl(imageId)}`}
+                  </a>
+                </li>
+              </ul>
+            )}
+        </CardBody>
+      </Collapse>
+    </Card>
+  );
+};
+
+TranscodeableCard.propTypes = {
+  node: PropTypes.instanceOf(Message).isRequired,
+};
+
+export default TranscodeableCard;
---
+++ src/plugins/ovp/components/transcribeable-card/index.jsx
@@ -0,0 +1,72 @@
+import { Button, Card, Collapse, CardBody, CardHeader } from '@triniti/admin-ui-plugin/components';
+import artifactUrl from '@triniti/cms/plugins/ovp/utils/artifactUrl';
+import AssetId from '@triniti/schemas/triniti/dam/AssetId';
+import camelCase from 'lodash/camelCase';
+import damUrl from '@triniti/cms/plugins/dam/utils/damUrl';
+import Message from '@gdbots/pbj/Message';
+import NodeRef from '@gdbots/schemas/gdbots/ncr/NodeRef';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import startCase from 'lodash/startCase';
+
+import './styles.scss';
+
+const TranscribeableCard = ({ node }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const status = node.has('transcription_status') ? node.get('transcription_status').getValue() : 'unknown';
+  const videoId = AssetId.fromString(NodeRef.fromNode(node).getId());
+  const documentId = AssetId.fromString(`document_vtt_${videoId.getDate()}_${videoId.getUuid()}`);
+
+  return (
+    <Card key={node.get('_id')} className="transcribable-card">
+      <CardHeader toggler className="pr-2">
+        <Button color="toggler" onClick={() => setIsOpen(!isOpen)} active={isOpen}>Transcribing</Button>
+        <div>
+          <small className="text-uppercase status-copy mr-0 pr-0">
+            status:
+          </small>
+          <small className={`text-uppercase status-copy mr-2 status-${status}`}>
+            {status}
+          </small>
+        </div>
+      </CardHeader>
+      <Collapse isOpen={isOpen}>
+        <CardBody className="pb-3">
+          {status !== 'completed'
+            ? <p>{`No artifacts available. Transcribing status: ${status}.`}</p>
+            : (
+              <ul className="pl-4">
+                {['audio', 'transcription'].map((type) => (
+                  <li className="mb-1">{`${startCase(type)}:`}
+                    <a
+                      href={artifactUrl(node, type)}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {` ${artifactUrl(node, type)}`}
+                    </a>
+                  </li>
+                ))}
+                <li className="mb-1">Caption:
+                  <a
+                    href={damUrl(documentId)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {` ${damUrl(documentId)}`}
+                  </a>
+                </li>
+              </ul>
+            )}
+        </CardBody>
+      </Collapse>
+    </Card>
+  );
+};
+
+TranscribeableCard.propTypes = {
+  node: PropTypes.instanceOf(Message).isRequired,
+};
+
+export default TranscribeableCard;
