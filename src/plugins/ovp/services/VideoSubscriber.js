6bf2aff0794fce59bd0ac70e6d912f078747ad7d
Richard Sumilang <rsumilang@users.noreply.github.com>
2020-09-10 10:51:08 -0700

    Recommendations enabled (#248)
    * Adding support for recommendations_enabled field.

--- src/plugins/ovp/services/VideoSubscriber.js
+++ src/plugins/ovp/services/VideoSubscriber.js
@@ -41,6 +41,7 @@ export default class VideoSubscriber extends EventSubscriber {
       'original_air_date',
       'related_videos_heading',
       'sharing_enabled',
+      'recommendations_enabled',
       'show_related_videos',
       'title',
       'youtube_custom_id',
@@ -192,6 +193,7 @@ export default class VideoSubscriber extends EventSubscriber {
       'is_live',
       'is_full_episode',
       'is_promo',
+      'recommendations_enabled',
       'sharing_enabled',
       'show_related_videos',
     ].forEach((fieldName) => {
