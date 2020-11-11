59d484e2f0d32002f9beb70e29bc773bd8813e48
Eric Jacobson <contact@efjacobson.com>
2020-11-11 14:29:04 -0800

    simple window.onerror

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -13,6 +13,20 @@ import App from './App';
 import preloadedState from './config/preloadedState';
 import Root from './Root';
 //
+
+window.onerror = (message, source, lineno, colno, err) => {
+  const formattedPayload = JSON.stringify({
+    message,
+    source,
+    lineno,
+    colno,
+    error: JSON.stringify(err, Object.getOwnPropertyNames(err)),
+  });
+  // todo: integrate with reporting service and probably put in a new place
+  console.error(`[window.onerror] ${formattedPayload}`);
+  return false; // still fire the default event handler
+};
+
 const app = new App(preloadedState);
 setInstance(app);
 app.start();
