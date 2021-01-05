186af50d59295dfddceb31e12ecdad1748bb6e4f
joel.capillo <joel.capillo@tmz.com>
2021-01-05 15:27:16 -0800

    combine in one exclusion

--- src/plugins/blocksmith/components/facebook-post-block-modal/getFacebookPostUrl.js
+++ src/plugins/blocksmith/components/facebook-post-block-modal/getFacebookPostUrl.js
@@ -1,7 +1,7 @@
 // supports list of valid Post Urls - https://developers.facebook.com/docs/plugins/oembed
 const getFacebookPostUrl = (str) => {
   try {
-    const regex = new RegExp('(?:(?:https?:)?\\/\\/)?(?:www\\.)?facebook\\.com\\/([^\\/]+\\/photo(\\.php|s|)(\\/|)|photo(\\.php|s|)(\\/|)|permalink\\.php(\\/|)|media\\/set(\\/|)|questions|notes|[^\\/]+\\/(activity|posts))[\\/?](set=|fbid=|[^\\/]+\\/[^\\/]+\\/|(?=.*\\bstory_fbid=\\b)(?=.*\\bid=\\b).+)?(?:[a-z0-9\\.]+\\/)?([0-9]+)\\/?(?:\\?.[^\\s][^"]*)?');
+    const regex = new RegExp('(?:(?:https?:)?\\/\\/)?(?:www\\.)?facebook\\.com\\/([^\\/]+\\/photo(\\.php|s|)(\\/|)|photo(\\.php|s|)(\\/|)|permalink\\.php(\\/|)|media\\/set(\\/|)|questions|notes|[^\\/]+\\/(activity|posts))[\\/?](set=|fbid=|[^\\/]+\\/[^\\/]+\\/|(?=.*\\bstory_fbid=\\b)(?=.*\\bid=\\b).+)?(?:[a-z0-9\\.]+\\/)?([0-9]+)\\/?(?:\\?.[^\\s"]*)?');
     const postUrlMatches = decodeURIComponent(str).match(regex);
     return postUrlMatches ? postUrlMatches[0] : null;
   } catch (e) {
