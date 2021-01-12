c43272b772cf981381753ebf85b8613197cbfb02
Arun Karnati <arun.karnati@gmail.com>
2021-01-11 17:35:59 -0800

    cleanup(util): getCoordinatesFromAddress, with teeny tiny JS updates, and export the anonymous fn

--- src/plugins/blocksmith/components/google-map-block-modal/getCoordinatesFromAddress.js
+++ src/plugins/blocksmith/components/google-map-block-modal/getCoordinatesFromAddress.js
@@ -3,27 +3,29 @@
  * Converts address to geographic coordinates using Google Geocoding API
  *
  * @param {String} str
+ *
  * @returns {String|null}
  */
-const getCoordinatesFromAddress = async (str) => {
+export default async (str) => {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(str)}&key=${GOOGLE_MAPS_API_KEY}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    const status = data.status;
+    const { error_message: error } = data;
 
-    if (status !== 'OK') {
-      console.error('Geocoding failed:', status);
+    if (error) {
+      console.error(`Geocoding failed with error: ${error}`);
+
       return null;
     }
 
-    const results = data.results[0];
-    return `${results.geometry.location.lat},${results.geometry.location.lng}`;
+    const { lat, lng } = data.results[0].geometry.location;
+
+    return `${lat},${lng}`;
   } catch (error) {
     console.error('Geocoding failed:', error);
+
     return null;
   }
 };
-
-export default getCoordinatesFromAddress;
