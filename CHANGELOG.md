78d011b0f6be242905e3c20273c5c4ef62c814bf
Alon Cohen <alon@cohencodes.com>
2021-01-27 17:34:30 -0800

    add ignore button back to stale data popup when in view mode (#287)
    * add ignore button back to stale data popup when in view mode
    
    * reverse buttons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 
 ## vN.N.N
+* Add ignore button back to stale data popup when in view mode
 * Implement error logging to logstream for raven and blocksmith errors
 * Set center field on Google Map Block using Geocoding API.
 * facebook embed code considered invalid for facebook post block
--- src/plugins/raven/services/NodeChangeWatcher.js
+++ src/plugins/raven/services/NodeChangeWatcher.js
@@ -88,6 +88,8 @@ export default class NodeChangeWatcher extends EventSubscriber {
         allowOutsideClick: false,
         confirmButtonText: `Refresh ${nodeRef.getLabel()}`,
         html: `This ${nodeRef.getLabel()} has been changed by <strong>${username}</strong> <em>(${schema.getCurie()})</em>.`,
+        showCancelButton: true,
+        cancelButtonText: 'Ignore',
         reverseButtons: true,
         title: 'STALE DATA',
         type: 'warning',
