cd913b90a169f61cfac761c19aaec241669ee75c
Albert <albertgcsula@gmail.com>
2021-05-14 10:37:09 -0700

    fix test

--- src/plugins/blocksmith/components/eme-form-block-modal/getEmeFormBlockRef.test.js
+++ src/plugins/blocksmith/components/eme-form-block-modal/getEmeFormBlockRef.test.js
@@ -2,9 +2,9 @@ import test from 'tape';
 import getEmeFormBlockRef from './getEmeFormBlockRef';
 
 test('EmeFormBlock', (t) => {
-  const formRef = '103bbf32-f3b4-415e-a85c-0735db7ca63d';
+  const formRef = 'eme:solicit:103bbf32-f3b4-415e-a85c-0735db7ca73d';
   const actual = getEmeFormBlockRef(formRef);
-  const expected = '103bbf32-f3b4-415e-a85c-0735db7ca63d';
+  const expected = 'eme:solicit:103bbf32-f3b4-415e-a85c-0735db7ca73d';
   t.deepEqual(actual, expected, 'It should accept an EME form ref.');
   t.end();
 });
