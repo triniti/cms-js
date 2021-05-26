88b4650cff0690f0e7e2383b8a7caa582a8b6ed7
Richard Sumilang <rsumilang@users.noreply.github.com>
2021-05-26 16:52:21 -0700

    SEO desc lengths (#319)
    * SEO desc lengths

---
+++ src/plugins/common/components/seo-fields/MetaDescriptionField.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormGroup, FormText, Input, Label } from '@triniti/admin-ui-plugin/components';
+
+const MetaDescriptionField = ({
+  input, label, meta: { touched, error }, readOnly, hasBorder, rows, maxLength, ...rest
+}) => {
+  const readOnlyField = readOnly ? 'readonly' : null;
+  const borderClass = hasBorder ? 'has-border' : null;
+
+  const value = input.value || '';
+  const metaDescriptionLength = value.length;
+  let metaDescriptionStyle = 'info';
+  if (metaDescriptionLength >= 160) {
+    metaDescriptionStyle = 'danger';
+  } else if (metaDescriptionLength >= 140) {
+    metaDescriptionStyle = 'warning';
+  }
+
+  return (
+    <FormGroup className={borderClass}>
+      {label && <Label for={input.name}>{label}</Label>}
+      <Input
+        id={input.name}
+        valid={touched && !error}
+        invalid={touched && !!error}
+        {...input}
+        {...rest}
+        type="textarea"
+        rows={rows}
+        readOnly={readOnlyField}
+        disabled={readOnly}
+      />
+      <FormText color={metaDescriptionStyle} className="ml-1">
+        {maxLength - metaDescriptionLength} characters remaining.
+      </FormText>
+    </FormGroup>
+  );
+};
+
+MetaDescriptionField.propTypes = {
+  input: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  maxLength: PropTypes.number,
+  meta: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  readOnly: PropTypes.bool,
+  hasBorder: PropTypes.bool,
+  rows: PropTypes.number,
+};
+
+MetaDescriptionField.defaultProps = {
+  label: '',
+  maxLength: 500,
+  readOnly: false,
+  hasBorder: false,
+  rows: 5,
+};
+
+export default MetaDescriptionField;
--- src/plugins/common/services/SeoSubscriber.js
+++ src/plugins/common/services/SeoSubscriber.js
@@ -1,17 +1,12 @@
 import camelCase from 'lodash/camelCase';
 import isUndefined from 'lodash/isUndefined';
 import EventSubscriber from '@gdbots/pbjx/EventSubscriber';
-import createTextCharacterCountTips from '@triniti/cms/utils/createTextCharacterCountTips';
 import NodeRef from '@gdbots/schemas/gdbots/ncr/NodeRef';
-import { fieldRules } from '../constants';
-
-const { DESCRIPTION_MAX_CHARACTERS, DESCRIPTION_WARNING_CHARACTERS } = fieldRules;
 
 export default class SeoSubscriber extends EventSubscriber {
   constructor() {
     super();
     this.onInitForm = this.onInitForm.bind(this);
-    this.onWarnForm = this.onWarnForm.bind(this);
     this.onSubmitForm = this.onSubmitForm.bind(this);
   }
 
@@ -33,24 +28,6 @@ export default class SeoSubscriber extends EventSubscriber {
 
     data.seoImageRef = node.has('seo_image_ref') ? node.get('seo_image_ref').toString() : null;
     data.metaKeywords = node.get('meta_keywords', []).map((keyword) => ({ label: keyword, value: keyword }));
-  }
-
-  /**
-   * Occurs before validation and is used to add warnings.
-   *
-   * @param {FormEvent} formEvent
-   */
-  onWarnForm(formEvent) {
-    const data = formEvent.getData();
-    const { metaDescription } = data;
-    const metaDescriptionWarning = createTextCharacterCountTips(
-      metaDescription,
-      DESCRIPTION_MAX_CHARACTERS,
-      DESCRIPTION_WARNING_CHARACTERS,
-    );
-    if (metaDescriptionWarning) {
-      formEvent.addWarning('metaDescription', metaDescriptionWarning);
-    }
   }
 
   /**
@@ -85,7 +62,6 @@ export default class SeoSubscriber extends EventSubscriber {
   getSubscribedEvents() {
     return {
       'triniti:common:mixin:seo.init_form': this.onInitForm,
-      'triniti:common:mixin:seo.warn_form': this.onWarnForm,
       'triniti:common:mixin:seo.submit_form': this.onSubmitForm,
     };
   }
