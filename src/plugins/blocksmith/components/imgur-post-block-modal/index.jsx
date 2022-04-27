095276a6d2dfc9fef65c0b7fed8174d6fc7a9cbb
Donte Burney <digitalartcrew@gmail.com>
2022-04-26 18:13:16 -1000

    imgur working

--- src/plugins/blocksmith/components/imgur-post-block-modal/index.jsx
+++ src/plugins/blocksmith/components/imgur-post-block-modal/index.jsx
@@ -69,10 +69,19 @@ export default class ImgurPostBlockModal extends React.Component {
     this.inputElement.focus();
 
     window.addEventListener('mouseout', this.handleMouseOut, true);
+
+    if(!this.props.isFreshBlock){
+      document.querySelector('.modal').addEventListener('mousemove', this.handleMouseOut, true);
+    }
+  
   }
 
   componentWillUnmount() {
-    window.removeEventListener('mouseout', this.handleMouseOut, true);
+   window.removeEventListener('mouseout', this.handleMouseOut, true);
+
+   if(!this.props.isFreshBlock){
+      document.querySelector('.modal').removeEventListener('mousemove', this.handleMouseOut, true);
+    }
   }
 
   setBlock() {
