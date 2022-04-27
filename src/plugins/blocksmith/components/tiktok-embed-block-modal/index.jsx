1aa9d6d25033486da208c702be898b4bf17c7eff
Donte Burney <digitalartcrew@gmail.com>
2022-04-26 18:21:17 -1000

    tik tok

--- src/plugins/blocksmith/components/tiktok-embed-block-modal/index.jsx
+++ src/plugins/blocksmith/components/tiktok-embed-block-modal/index.jsx
@@ -64,12 +64,20 @@ export default class TikTokEmbedBlockModal extends React.Component {
     this.inputElement.focus();
 
     window.addEventListener('mouseout', this.handleMouseOut, true);
+  
+    if (!this.props.isFreshBlock) {
+      document.querySelector('.modal').addEventListener('mousemove', this.handleMouseOut, true);
+    } 
+   
   }
 
   componentWillUnmount() {
-    window.removeEventListener('mouseout', this.handleMouseOut, true);
-  }
+   window.removeEventListener('mouseout', this.handleMouseOut, true);
 
+    if (!this.props.isFreshBlock) {
+      document.querySelector('.modal').removeEventListener('mousemove', this.handleMouseOut, true);
+    } 
+  }
   setBlock() {
     const { hasUpdatedDate,
       tiktokId,
