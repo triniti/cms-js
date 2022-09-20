9b319f993b23ee26cb09476a5080bd8e0650929e
meghedi <hacopian.m@gmail.com>
2022-09-20 15:17:43 -0700

    asset details -wip

--- src/plugins/dam/components/asset-screen/VideoAssetFields.jsx
+++ src/plugins/dam/components/asset-screen/VideoAssetFields.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+import { TextField } from 'components';
+import humanizeBytes from 'utils/humanizeBytes';
 import artifactUrl from 'plugins/ovp/artifactUrl';
 import ReactPlayer from 'react-player';
-import classNames from 'classnames';
 import CommonFields from './CommonFields';
 
 
 export default function VideoAssetFields(props) {
-  const {asset, groupClassName = ''} = props  
+  const { asset } = props  
 
-  const rootClassName = classNames(
-    groupClassName,
-     'form-group',
-  );  
   return (
-    <div className={rootClassName}>
-     <ReactPlayer url={artifactUrl(asset, 'video')} width="100%" height="auto" controls />
-     <CommonFields asset={asset} credit="video-asset-credits" />
-    </div>
+    <>
+      <Container fluid className="ui-cols">
+        <Row>
+          <Col xs="6 ps-0">
+            <TextField name="mime_type" label="MIME type" readOnly />
+            <TextField name="file_size" label="File size" format={humanizeBytes} readOnly />
+          </Col>
+          <Col xs="6 pe-0">
+            <ReactPlayer url={artifactUrl(asset, 'video')} width="100%" height="auto" controls />
+          </Col>
+        </Row>
+      </Container>
+      <CommonFields asset={asset} credit="video-asset-credits" />
+    </>
   );
 }
