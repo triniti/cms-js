8fdb8ef2eb1f2ba877f382e2aaca347afb3153dc
meghedi <hacopian.m@gmail.com>
2022-09-22 18:53:15 -0700

    remove react image magnify

--- src/plugins/dam/components/asset-screen/ImageAssetFields.jsx
+++ src/plugins/dam/components/asset-screen/ImageAssetFields.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactImageMagnify from 'react-image-magnify';
-import { Container, Col, Row, Spinner } from 'reactstrap';
+import { Container, Col, Media, Row } from 'reactstrap';
 import { TextField } from 'components';
 import damUrl from 'plugins/dam/damUrl';
 import humanizeBytes from 'utils/humanizeBytes';
@@ -12,7 +11,7 @@ export default function ImageAssetFields({ asset }) {
     return `${value} x ${asset.get('height')}`;
   }
 
-  const previewUrl = damUrl(asset, 'o');
+  const previewUrl = damUrl(asset);
 
   return (
     <>
@@ -24,46 +23,16 @@ export default function ImageAssetFields({ asset }) {
             <TextField name="width" label="Dimensions" format={getDimensions} readOnly />
           </Col>
           <Col xs="6 pe-0">
-            {!previewUrl && <Spinner centered />}
             {previewUrl && (
-                <ReactImageMagnify
-                  smallImage={{
-                    src: previewUrl,
-                    isFluidWidth: true,
-                  }}
-                  imageStyle={{
-                    objectFit: 'contain',
-                  }}
-                  largeImage={{
-                    src: previewUrl,
-                    width: (asset.get('width')),
-                    height: (asset.get('height')),
-                  }}
-                  enlargedImageContainerStyle={{
-                    position: 'fixed',
-                    top: '50%',
-                    left: '50%',
-                    border: '0px',
-                    marginLeft: 0,
-                    maxHeight: '100vh',
-                    maxWidth: '100vw',
-                    transform: 'translate(-50%, -50%)',
-                    zIndex: 1049,
-                    pointerEvents: 'none',
-                  }}
-                  enlargedImagePosition="beside"
-                  enlargedImageContainerDimensions={{ width: '100%' }}
-                  enlargedImageContainerClassName="w-auto"
-                  enlargedImageStyle={{
-                    margin: '16px',
-                    border: '0px',
-                    maxHeight: 'calc(90vh)',
-                    maxWidth: 'calc(90vw)',
-                    filter: 'drop-shadow(0 1px 4px rgba(0,0,0,0.4)',
-                    objectFit: 'scale-down',
-                  }}
-                  enlargedImageClassName="h-100 w-auto asset-hover-mw"
+              <a href={previewUrl} target="_blank" rel="noopener noreferrer">
+                <Media src={previewUrl}
+                  alt=""
+                  width="100%"
+                  height="auto"
+                  object
+                  className="rounded-2"
                 />
+              </a>
             )}
           </Col>
         </Row>
