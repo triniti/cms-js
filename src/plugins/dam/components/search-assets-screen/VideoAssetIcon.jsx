fe41a9bd2cf0b49eda162eae0d7344a6ce9be504
meghedi <hacopian.m@gmail.com>
2022-10-06 15:42:20 -0700

    video asset button

--- src/plugins/dam/components/search-assets-screen/VideoAssetIcon.jsx
+++ src/plugins/dam/components/search-assets-screen/VideoAssetIcon.jsx
@@ -3,6 +3,7 @@ import ReactPlayer from 'react-player';
 import { Button } from 'reactstrap';
 import Icon from 'components/icon';
 import artifactUrl from 'plugins/ovp/artifactUrl';
+import VideoAssetButton from './VideoAssetButton';
 
 export default function VideoAssetIcon({ asset }) {
 
@@ -11,6 +12,8 @@ export default function VideoAssetIcon({ asset }) {
     url: '',
     volume: 1.0,
     width: '0px',
+    isPlaying: false,
+    currentlyPlayingAssetId: null
   });
 
   const playerRef = useRef(null);
@@ -87,24 +90,11 @@ export default function VideoAssetIcon({ asset }) {
           style={{ filter: 'drop-shadow(0 1px 4px rgba(0,0,0,0.4)' }}
         />
       </div>
-      <Button
-        radius="circle"
-        size="sm"
-        color="hover"
-        onClick={() => { 
-          handleMediaPlayerCommand('play', asset, 'video', { width: '100%', height: '100%' });
-          //window.open(artifactUrl(asset, 'video'));
-        }}
-        id={`button.${asset.get('_id').toString()}`}
-        className={`mt-1 mb-0`}
-      >
-        <span className="m-0 icon-toggle icon icon-lg">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-3-6V8a.5.5 0 0 1 .736-.44l6.67 3.942a.5.5 0 0 1 .041.857l-6.67 4.058A.5.5 0 0 1 9 16z" /></svg>
-        </span>
-        <span className="m-0 icon-toggle icon icon-lg">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM7.5 7h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm7 0h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5z" /></svg>
-        </span>
-      </Button>
+      <VideoAssetButton
+            asset={asset}
+            currentlyPlayingAssetId={mediaPlayer.currentlyPlayingAssetId}
+            onPlayerCommand={handleMediaPlayerCommand}
+          />
     </>
   );
 }
