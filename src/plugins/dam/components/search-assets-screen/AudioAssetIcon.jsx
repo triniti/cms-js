8cbfb83cd14780c29d59b0712c670f29900169a9
meghedi <hacopian.m@gmail.com>
2022-10-12 12:15:16 -0700

    audio Asset Icon added

--- src/plugins/dam/components/search-assets-screen/AudioAssetIcon.jsx
+++ src/plugins/dam/components/search-assets-screen/AudioAssetIcon.jsx
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import ReactPlayer from 'react-player';
 import { Button } from 'reactstrap';
+import artifactUrl from 'plugins/ovp/artifactUrl';
 
-export default function AudioAssetIcon({asset}) {
+export default function VideoAssetIcon({ asset }) {
+  const playerRef = useRef(null);
+
+  const [mediaPlayer, setMediaPlayer] = useState({
+    controls: false,
+    currentlyPlayingAssetId: null,
+    height: '0px',
+    isPlaying: false,
+    mediaType: null,
+    url: '',
+    volume: 1.0,
+    width: '0px',
+  });
+
+
+  const handleMediaPlayerCommand = (command, asset, mediaType, dimensions = { width: 0, height: 0 }) => {
+    playerRef.current?.seekTo(0);
+    let currentlyPlayingAssetId = null;
+    let currentDimensions = { width: 0, height: 0 };
+    let isPlaying = false;
+    if (command === 'play') {
+      currentlyPlayingAssetId = asset.get('_id');
+      isPlaying = true;
+      currentDimensions = dimensions;
+    }
+
+    setMediaPlayer({
+      controls: false,
+      currentlyPlayingAssetId,
+      height: currentDimensions.height,
+      isPlaying,
+      mediaType: mediaType || null,
+      url: asset ? artifactUrl(asset, 'video') : null,
+      volume: mediaPlayer.volume,
+      width: currentDimensions.width,
+    });
+  }
+
+  const playing = mediaPlayer.currentlyPlayingAssetId !== null && asset.get('_id').toString() === mediaPlayer.currentlyPlayingAssetId.toString();
+  const buttonState = playing ? 'active' : '';
+  const command = playing ? 'stop' : 'play';
+
   return (
-    <Button
+    <>
+      <div>
+        <ReactPlayer
+          controls={mediaPlayer.controls}
+          height={mediaPlayer.height}
+          playing={mediaPlayer.isPlaying}
+          ref={playerRef}
+          url={mediaPlayer.url}
+          volume={mediaPlayer.volume}
+          width={mediaPlayer.width}
+          className="embed-responsive embed-responsive-16by9"
+          style={{ filter: 'drop-shadow(0 1px 4px rgba(0,0,0,0.4)' }}
+        />
+      </div>
+      <Button
         radius="circle"
         size="sm"
         color="hover"
         id={`button.${asset.get('_id').toString()}`}
-        className={`mt-1 mb-0`}
-    >
+        onClick={() => handleMediaPlayerCommand(command, asset, 'audio')}
+        className={`mt-1 mb-0 ${buttonState}`}
+      >
         <span className="m-0 icon-toggle icon icon-lg">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-3-6V8a.5.5 0 0 1 .736-.44l6.67 3.942a.5.5 0 0 1 .041.857l-6.67 4.058A.5.5 0 0 1 9 16z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-3-6V8a.5.5 0 0 1 .736-.44l6.67 3.942a.5.5 0 0 1 .041.857l-6.67 4.058A.5.5 0 0 1 9 16z" /></svg>
         </span>
         <span className="m-0 icon-toggle icon icon-lg">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM7.5 7h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm7 0h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM7.5 7h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm7 0h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5z" /></svg>
         </span>
-    </Button>
+      </Button>
+    </>
   );
 }
--- src/plugins/dam/components/search-assets-screen/VideoAssetIcon.jsx
+++ src/plugins/dam/components/search-assets-screen/VideoAssetIcon.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactPlayer from 'react-player';
 import { Button } from 'reactstrap';
 import Icon from 'components/icon';
 import artifactUrl from 'plugins/ovp/artifactUrl';
-import VideoAssetButton from './VideoAssetButton';
 
 export default function VideoAssetIcon({ asset }) {
   const playerRef = useRef(null);
-  
+
   const [mediaPlayer, setMediaPlayer] = useState({
     controls: false,
     currentlyPlayingAssetId: null,
@@ -47,6 +46,10 @@ export default function VideoAssetIcon({ asset }) {
     });
   }
 
+  const playing = mediaPlayer.currentlyPlayingAssetId !== null && asset.get('_id').toString() === mediaPlayer.currentlyPlayingAssetId.toString();
+  const buttonState = playing ? 'active' : '';
+  const command = playing ? 'stop' : 'play';
+
   return (
     <>
       <div
@@ -62,24 +65,21 @@ export default function VideoAssetIcon({ asset }) {
         }}
       >
         {mediaPlayer.isPlaying && mediaPlayer.mediaType === 'video' && (
-            <>
-              <Icon
-                id="dismiss-player"
-                imgSrc="delete"
-                alert
-                onClick={() => handleMediaPlayerCommand('stop')}
-                size="xs"
-                style={{
-                  cursor: 'pointer',
-                  position: 'absolute',
-                  right: '2px',
-                  top: '2px',
-                  zIndex: 1,
-                }}
-              />
-            </>
-          )
-        }
+          <Icon
+            id="dismiss-player"
+            imgSrc="delete"
+            alert
+            onClick={() => handleMediaPlayerCommand('stop')}
+            size="xs"
+            style={{
+              cursor: 'pointer',
+              position: 'absolute',
+              right: '2px',
+              top: '2px',
+              zIndex: 1,
+            }}
+          />
+        )}
         <ReactPlayer
           controls={mediaPlayer.controls}
           height={mediaPlayer.height}
@@ -92,11 +92,21 @@ export default function VideoAssetIcon({ asset }) {
           style={{ filter: 'drop-shadow(0 1px 4px rgba(0,0,0,0.4)' }}
         />
       </div>
-      <VideoAssetButton
-        asset={asset}
-        currentlyPlayingAssetId={mediaPlayer.currentlyPlayingAssetId}
-        onPlayerCommand={handleMediaPlayerCommand}
-      />
+      <Button
+        radius="circle"
+        size="sm"
+        color="hover"
+        id={`button.${asset.get('_id').toString()}`}
+        onClick={() => handleMediaPlayerCommand(command, asset, 'video', { width: '100%', height: '100%' })}
+        className={`mt-1 mb-0 ${buttonState}`}
+      >
+        <span className="m-0 icon-toggle icon icon-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-3-6V8a.5.5 0 0 1 .736-.44l6.67 3.942a.5.5 0 0 1 .041.857l-6.67 4.058A.5.5 0 0 1 9 16z" /></svg>
+        </span>
+        <span className="m-0 icon-toggle icon icon-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM7.5 7h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm7 0h2a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5z" /></svg>
+        </span>
+      </Button>
     </>
   );
 }
