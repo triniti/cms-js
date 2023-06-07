dc4af73969cc46d55e158ddb97e9e3966b0078ae
Richard Sumilang <rsumilang@users.noreply.github.com>
2023-06-06 21:29:46 -0700

    Youtube video block

--- src/components/blocksmith-field/components/youtube-video-block-modal/index.jsx
+++ src/components/blocksmith-field/components/youtube-video-block-modal/index.jsx
@@ -3,14 +3,21 @@ import { ModalBody } from 'reactstrap';
 import { SwitchField, TextareaField, TextField } from 'components';
 import getYouTubeId from 'utils/getYouTubeId';
 import withBlockModal from 'components/blocksmith-field/components/with-block-modal';
+import Preview from 'components/blocksmith-field/components/youtube-video-block-modal/Preview';
+import ImagePickerField from 'plugins/dam/components/image-picker-field';
 
-function YoutubeVideoBlockModal() {
+function YoutubeVideoBlockModal(props) {
+  const { formState } = props;
+  const { valid } = formState;
+  const { id } = formState.values;
+
   return (
     <div className="modal-scrollable">
       <ModalBody>
         <TextareaField name="id" label="URL" placeholder="enter url or embed code" parse={getYouTubeId} />
-        <TextField name="start_at" label="Start At ( In Seconds )" value="0" />
-        {/* fixme: add image picker when it's ready */}
+        <TextField name="start_at" label="Start At ( In Seconds )" parse={(str) => str ? parseInt(str) : ''} placeholder="Time in seconds" />
+        {valid && <Preview {...props} />}
+        {id && <ImagePickerField name="poster_image_ref" previewImage={false} width={526} />}
         <SwitchField name="autoplay" label="Autoplay" />
         <SwitchField name="aside" label="Aside" tooltip="Is only indirectly related to the main content." />
       </ModalBody>
--- src/components/blocksmith-field/components/youtube-video-block-preview/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function YoutubeVideoBlockPreview({ }) {
-  return (
-    <div>
-      YouTube Video Block
-    </div>
-  );
-};
