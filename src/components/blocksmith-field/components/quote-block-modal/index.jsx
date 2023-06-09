5154fd135c6b43f478d516d8ebbf625175236f2a
Richard Sumilang <rsumilang@users.noreply.github.com>
2023-06-08 19:57:46 -0700

    quote block

--- src/components/blocksmith-field/components/quote-block-modal/index.jsx
+++ src/components/blocksmith-field/components/quote-block-modal/index.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { ModalBody } from 'reactstrap';
 import { SwitchField, TextareaField, TextField, UrlField } from 'components';
 import withBlockModal from 'components/blocksmith-field/components/with-block-modal';
+import Preview from './Preview';
 
-function QuoteBlockModal() {
+function QuoteBlockModal(props) {
+  const { formState } = props;
+  const { valid } = formState;
   return (
     <div className="modal-scrollable">
       <ModalBody>
         <TextareaField name="text" label="Text" placeholder="Enter the quoted text here" />
         <TextField name="source" label="Source" placeholder="Source" />
         <UrlField name="source_url" label="Source URL" placeholder="Source URL" />
-        <SwitchField name="pull_quote" label="Pull Quote" editMode />
+        <SwitchField name="is_pull_quote" label="Pull Quote" />
+        {valid && <Preview {...props} />}
       </ModalBody>
     </div>
   );
