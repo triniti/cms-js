e162e4dccb8998d4b04dcb241bdbe8cc53df43d9
Richard Sumilang <rsumilang@users.noreply.github.com>
2023-10-17 07:56:39 -0700

    icon

--- src/plugins/curator/components/promotion-screen/SortableSlots.jsx
+++ src/plugins/curator/components/promotion-screen/SortableSlots.jsx
@@ -21,6 +21,9 @@ import {
 } from "@dnd-kit/sortable";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import {
+  Icon,
+} from '@triniti/cms/components';
 
 import './SortableSlots.scss';
 
@@ -30,9 +33,7 @@ const DragHandle = () =>  {
   const { attributes, listeners, ref } = useContext(SortableItemContext);
   return (
     <button className="DragHandle" {...attributes} {...listeners} ref={ref}>
-      <svg viewBox="0 0 20 20" width="12">
-        <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
-      </svg>
+      <Icon imgSrc="drag" />
     </button>
   );
 }
