bafc38abfe95596e1978a4d35d7b6c7843a7335c
meghedi <hacopian.m@gmail.com>
2024-02-05 14:47:15 -0800

    add search for channels

--- src/plugins/taxonomy/components/search-channels-screen/index.jsx
+++ src/plugins/taxonomy/components/search-channels-screen/index.jsx
@@ -1,5 +1,5 @@
 import React, { lazy } from 'react';
-import { Button, Card, Table } from 'reactstrap';
+import { Badge, Button, Card, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import SearchChannelsSort from '@triniti/schemas/triniti/taxonomy/enums/SearchChannelsSort';
 import { CreateModalButton, Icon, Loading, Screen } from 'components';
@@ -7,14 +7,16 @@ import nodeUrl from 'plugins/ncr/nodeUrl';
 import useRequest from 'plugins/pbjx/components/useRequest';
 import withRequest from 'plugins/pbjx/components/with-request';
 import usePolicy from 'plugins/iam/components/usePolicy';
+import SearchForm from 'plugins/taxonomy/components/search-channels-screen/SearchForm';
 import Collaborators from 'plugins/raven/components/collaborators';
 import NodeRef from '@gdbots/pbj/well-known/NodeRef';
+import withForm from '../../../../components/with-form';
 
 const CreateChannelModal = lazy(() => import('plugins/taxonomy/components/create-channel-modal'));
 
 function SearchChannelsScreen(props) {
   const { request } = props;
-  const { response, pbjxError } = useRequest(request, true);
+  const { response, run, isRunning, pbjxError } = useRequest(request, true);
   const policy = usePolicy();
   const canCreate = policy.isGranted(`${APP_VENDOR}:channel:create`);
   const canUpdate = policy.isGranted(`${APP_VENDOR}:channel:update`);
@@ -25,10 +27,12 @@ function SearchChannelsScreen(props) {
       header="Channels"
       primaryActions={
         <>
+          {isRunning && <Badge color="light" pill><span className="badge-animated">Searching</span></Badge>}
           {canCreate && <CreateModalButton text="Create Channel" modal={CreateChannelModal} />}
         </>
       }
     >
+      <SearchForm {...props} isRunning={isRunning} run={run} />
       {(!response || pbjxError) && <Loading error={pbjxError} />}
 
       {response && response.has('nodes') && (
@@ -67,7 +71,7 @@ function SearchChannelsScreen(props) {
   );
 }
 
-export default withRequest(SearchChannelsScreen, 'triniti:taxonomy:request:search-channels-request', {
+export default withRequest(withForm(SearchChannelsScreen), 'triniti:taxonomy:request:search-channels-request', {
   persist: true,
   initialData: {
     count: 50,
