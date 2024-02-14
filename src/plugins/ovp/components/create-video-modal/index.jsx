641b8daa19986d11fce796276a5f4c5354ccad4e
meghedi <hacopian.m@gmail.com>
2024-02-14 09:59:54 -0800

    add datedSlug for videos

--- src/plugins/ovp/components/create-video-modal/index.jsx
+++ src/plugins/ovp/components/create-video-modal/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { FORM_ERROR } from 'final-form';
@@ -9,10 +9,19 @@ import progressIndicator from 'utils/progressIndicator';
 import toast from 'utils/toast';
 import getFriendlyErrorMessage from 'plugins/pbjx/utils/getFriendlyErrorMessage';
 import nodeUrl from 'plugins/ncr/nodeUrl';
+import createSlug from '@gdbots/pbj/utils/createSlug';
+import { addDateToSlug } from '@gdbots/pbj/utils';
+import isValidSlug from '@gdbots/pbj/utils/isValidSlug';
+import trimStart from 'lodash-es/trimStart';
+
+// more restrictive DATED_SLUG_PATTERN than what gdbots/pbj does
+const DATED_SLUG_PATTERN = /^\d{4}\/\d{2}\/\d{2}\/[a-z0-9-]+$/;
+const validDatedSlug = value => isValidSlug(value, true) && DATED_SLUG_PATTERN.test(trimStart(value)) ? true : false;
 
 function CreateVideoModal(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [slug, setSlug] = useState('');
 
   const { delegate, form, formState, handleSubmit, pbj } = props;
   const { dirty, hasSubmitErrors, submitErrors, submitting, valid } = formState;
@@ -22,6 +31,13 @@ function CreateVideoModal(props) {
   delegate.handleSubmit = async (values) => {
     try {
       await progressIndicator.show('Creating Video...');
+      if(slug && validDatedSlug(slug)){
+        values.slug = slug;
+      }else if(slug && !validDatedSlug(slug)){
+        values.slug = addDateToSlug(slug);
+      } else {
+        values.slug =  addDateToSlug(createSlug(values.title, true));
+      }
       await dispatch(createNode(values, form, pbj));
       props.toggle();
       await progressIndicator.close();
@@ -33,13 +49,30 @@ function CreateVideoModal(props) {
     }
   };
 
+  const handleBlur = e => {
+    if(e.target.value) {
+      setSlug(addDateToSlug(createSlug(e.target.value, true)));
+    }
+  }
+
+  const handleKeyDown = e =>{
+    if (e.key === 'Enter' && e.target.value) {
+      setSlug(addDateToSlug(createSlug(e.target.value, true)));
+    }
+  }
+
+  const handleChange = e => {
+    setSlug(e.target.value);
+  }
+
   return (
     <Modal isOpen backdrop="static">
       <ModalHeader toggle={props.toggle}>Create Video</ModalHeader>
       {hasSubmitErrors && <FormErrors errors={submitErrors} />}
       <ModalBody className="modal-scrollable">
         <Form onSubmit={handleSubmit} autoComplete="off">
-          <TextField name="title" label="Title" required />
+          <TextField name="title" label="Title" onBlur={handleBlur} onKeyDown={handleKeyDown} required />
+          <TextField name="slug" label="Slug" value={slug} onChange={handleChange} />
         </Form>
       </ModalBody>
       <ModalFooter>
