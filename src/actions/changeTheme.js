4d1c17146f809c939d29de2cece4251f2e6cd394
mgollnick <mgollnick@gmail.com>
2024-03-01 17:00:37 -0800

    - Remove lastTheme from changeTheme

--- src/actions/changeTheme.js
+++ src/actions/changeTheme.js
@@ -1,7 +1,6 @@
 import { actionTypes, THEME_STORAGE_KEY } from 'constants';
 
 export default (theme) => {
-  const lastTheme = localStorage.getItem(THEME_STORAGE_KEY) || 'light';
   document.documentElement.setAttribute('data-bs-theme',(theme))
   localStorage.setItem(THEME_STORAGE_KEY, theme);
   return { type: actionTypes.THEME_CHANGED, theme };
