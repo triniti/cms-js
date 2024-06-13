0e5711bc7e4e66f9ed9df8210e12baa34d4c52e6
Greg Brown <greg@12dnetworks.com>
2024-06-13 14:57:20 -0700

    sync with eme improvements

--- src/plugins/common/components/seo-tab/index.js
+++ src/plugins/common/components/seo-tab/index.js
@@ -11,7 +11,7 @@ export default function SeoTab(props) {
       <CardBody>
         <TextField name="seo_title" label="SEO Title Tag" />
         <TextareaField name="meta_description" label="Meta Description" maxCharsConfig={{charsMax: 500}} parse={value => value && value.replace('\n', '')} />
-        <SelectField name="meta_keywords" label="Meta Keywords" allowOther isMulti={true} />
+        <SelectField name="meta_keywords" label="Meta Keywords" allowOther isMulti />
         <ImagePickerField name="seo_image_ref" label="Seo Image" nodeRef={nodeRef} />
         <DatePickerField name="seo_published_at" label="SEO Published At" />
         <SwitchField name="is_unlisted" label="Unlisted" />
