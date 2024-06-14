9c9c7b7d9678fa0a6f69b52fa10372d48d4f0c23
Greg Brown <greg@12dnetworks.com>
2024-06-14 12:47:01 -0700

    sync with eme improvements

--- src/components/date-picker-field/index.js
+++ src/components/date-picker-field/index.js
@@ -8,33 +8,41 @@ import { Icon, useField, useFormContext } from '@triniti/cms/components/index.js
 // fixme: handle date vs date-time scenarios
 export default function DatePickerField(props) {
   const {
-    groupClassName = '',
     name,
     label,
     description,
+    nestedPbj,
     pbjName,
+    groupClassName = '',
     isClearable = true,
     readOnly = false,
-    required = false,
-    showTimeSelect = true,
-    dateFormat = 'MM/dd/yyyy h:mm a',
+    required = false
   } = props;
   const formContext = useFormContext();
   const { editMode } = formContext;
-  const { input, meta } = useField({ ...props }, formContext);
+  const { input, meta, pbjField } = useField({ ...props }, formContext);
 
-  const rootClassName = classNames(
-    groupClassName,
-    'form-group',
-  );
-
+  const rootClassName = classNames(groupClassName, 'form-group');
   const className = classNames(
     'form-control',
     meta.touched && !meta.valid && 'is-invalid',
     meta.touched && meta.valid && 'is-valid',
   );
 
-  const currentDate = input.value ? new Date(input.value) : null;
+  let currentDate;
+  let dateOnly = false;
+
+  if (pbjField && pbjField.getType().getTypeValue() === 'date') {
+    dateOnly = true;
+    const [year, month, day] = (input.value || '').split('-').map(Number);
+    currentDate = input.value ? new Date(year, month - 1, day) : null;
+  } else {
+    currentDate = input.value ? new Date(input.value) : null;
+  }
+
+  const timeOptions = dateOnly
+    ? {dateFormat: 'MM/dd/yyyy'}
+    : {showTimeSelect: true, dateFormat: 'MM/dd/yyyy h:mm a', timeFormat : 'h:mm a', timeCaption: 'Time'};
 
   return (
     <div className={rootClassName} id={`form-group-${pbjName || name}`}>
@@ -51,20 +59,18 @@ export default function DatePickerField(props) {
             readOnly={!editMode}
             selected={currentDate}
             isClearable={isClearable}
-            showTimeSelect={showTimeSelect}
-            timeFormat="h:mm a"
-            timeCaption="Time"
-            dateFormat={dateFormat}
+            {...timeOptions}
             {...input}
             value={currentDate}
             onChange={date => {
-              input.onChange(date ? date.toISOString() : undefined);
+              const v = date ? (dateOnly ? date.toISOString().substring(0, 10) : date.toISOString()) : undefined;
+              input.onChange(v);
               input.onBlur();
             }}
           />
         )}
         {(!editMode || readOnly) && (
-          <input type="text" className="form-control" readOnly value={currentDate ? formatDate(currentDate) : ''} />
+          <input type="text" className="form-control" readOnly value={currentDate ? formatDate(currentDate, dateOnly ? 'MMM dd, yyyy' : undefined) : ''} />
         )}
       </InputGroup>
       {description && <FormText color="dark">{description}</FormText>}
