23067b80ea26b30c15eb04c6252074013c70ab9a
Greg Brown <greg@12dnetworks.com>
2024-06-18 12:31:29 -0700

    search-categories-screen updated

--- src/plugins/taxonomy/components/search-categories-screen/index.js
+++ src/plugins/taxonomy/components/search-categories-screen/index.js
@@ -1,17 +1,14 @@
 import React, { lazy } from 'react';
-import { Badge, Button, Card, Table } from 'reactstrap';
+import { Button, Card, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import SearchCategoriesSort from '@triniti/schemas/triniti/taxonomy/enums/SearchCategoriesSort.js';
 import { CreateModalButton, Icon, Loading, Pager, Screen, withForm } from '@triniti/cms/components/index.js';
-import { scrollToTop } from '@triniti/cms/components/screen/index.js';
 import nodeUrl from '@triniti/cms/plugins/ncr/nodeUrl.js';
 import useRequest from '@triniti/cms/plugins/pbjx/components/useRequest.js';
 import withRequest from '@triniti/cms/plugins/pbjx/components/with-request/index.js';
 import formatDate from '@triniti/cms/utils/formatDate.js';
 import usePolicy from '@triniti/cms/plugins/iam/components/usePolicy.js';
 import SearchForm from '@triniti/cms/plugins/taxonomy/components/search-categories-screen/SearchForm.js';
-import Collaborators from '@triniti/cms/plugins/raven/components/collaborators/index.js';
-import NodeRef from '@gdbots/pbj/well-known/NodeRef.js';
 
 const CreateCategoryModal = lazy(() => import('@triniti/cms/plugins/taxonomy/components/create-category-modal/index.js'));
 
@@ -22,21 +19,14 @@ function SearchCategoriesScreen(props) {
   const canCreate = policy.isGranted(`${APP_VENDOR}:category:create`);
   const canUpdate = policy.isGranted(`${APP_VENDOR}:category:update`);
 
-  delegate.handleChangePage = page => {
-    request.set('page', page);
-    run();
-    scrollToTop();
-  };
-
   return (
     <Screen
       title="Categories"
       header="Categories"
-      contentWidth="1600px"
+      contentWidth="1200px"
       primaryActions={
         <>
-          {isRunning && <Badge color="light" pill><span className="badge-animated">Searching</span></Badge>}
-          {canCreate && <CreateModalButton text="Create Category" modal={CreateCategoryModal} />}
+          {canCreate && <CreateModalButton text="Create Category" icon="plus-outline" modal={CreateCategoryModal} />}
         </>
       }
     >
@@ -51,44 +41,42 @@ function SearchCategoriesScreen(props) {
           </div>
 
           <Card>
-            <Table responsive>
+            <Table hover responsive>
               <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Created At</th>
-                  <th>Updated At</th>
-                  <th></th>
-                </tr>
+              <tr>
+                <th>Title</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+                <th></th>
+              </tr>
               </thead>
               <tbody>
-                {response.get('nodes', []).map(node => {
-                  return (
-                    <tr key={`${node.get('_id')}`} className={`status-${node.get('status')}`}>
-                      <td>{node.get('title')} <Collaborators nodeRef={NodeRef.fromNode(node)} /></td>
-                      <td className="text-nowrap">{formatDate(node.get('created_at'))}</td>
-                      <td className="text-nowrap">{formatDate(node.get('updated_at'))}</td>
-                      <td className="td-icons">
-                        <Link to={nodeUrl(node, 'view')}>
-                          <Button color="hover">
-                            <Icon imgSrc="eye" alt="view" />
-                          </Button>
-                        </Link>
-                        {canUpdate && (
-                          <Link to={nodeUrl(node, 'edit')}>
-                            <Button color="hover">
-                              <Icon imgSrc="pencil" alt="edit" />
-                            </Button>
-                          </Link>
-                        )}
-                        <a href={nodeUrl(node, 'canonical')} target="_blank" rel="noopener noreferrer">
-                          <Button color="hover">
-                            <Icon imgSrc="external" alt="open" />
-                          </Button>
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
+              {response.get('nodes', []).map(node => (
+                <tr key={`${node.get('_id')}`} className={`status-${node.get('status')}`}>
+                  <td>{node.get('title')}</td>
+                  <td className="text-nowrap">{formatDate(node.get('created_at'))}</td>
+                  <td className="text-nowrap">{formatDate(node.get('updated_at'))}</td>
+                  <td className="td-icons">
+                    <Link to={nodeUrl(node, 'view')}>
+                      <Button color="hover" tabIndex="-1">
+                        <Icon imgSrc="eye" alt="view" />
+                      </Button>
+                    </Link>
+                    {canUpdate && (
+                      <Link to={nodeUrl(node, 'edit')}>
+                        <Button color="hover" tabIndex="-1">
+                          <Icon imgSrc="pencil" alt="edit" />
+                        </Button>
+                      </Link>
+                    )}
+                    <a href={nodeUrl(node, 'canonical')} target="_blank" rel="noopener noreferrer">
+                      <Button color="hover">
+                        <Icon imgSrc="external" alt="open" />
+                      </Button>
+                    </a>
+                  </td>
+                </tr>
+              ))}
               </tbody>
             </Table>
           </Card>
@@ -111,5 +99,6 @@ export default withRequest(withForm(SearchCategoriesScreen), 'triniti:taxonomy:r
   persist: true,
   initialData: {
     sort: SearchCategoriesSort.TITLE_DESC.getValue(),
+    track_total_hits: true,
   }
 });
