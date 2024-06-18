2c680186f8dc72da7b661779d9b0906cf0327f7c
Greg Brown <greg@12dnetworks.com>
2024-06-18 13:42:12 -0700

    common plugin nits

--- src/plugins/common/components/seo-tab/index.js
+++ src/plugins/common/components/seo-tab/index.js
@@ -7,12 +7,12 @@ export default function SeoTab(props) {
   const { nodeRef } = props;
   return (
     <Card>
-      <CardHeader>Seo</CardHeader>
+      <CardHeader>SEO</CardHeader>
       <CardBody>
         <TextField name="seo_title" label="SEO Title Tag" />
         <TextareaField name="meta_description" label="Meta Description" maxCharsConfig={{charsMax: 500}} parse={value => value && value.replace('\n', '')} />
         <SelectField name="meta_keywords" label="Meta Keywords" allowOther isMulti />
-        <ImagePickerField name="seo_image_ref" label="Seo Image" nodeRef={nodeRef} />
+        <ImagePickerField name="seo_image_ref" label="SEO Image" nodeRef={nodeRef} />
         <DatePickerField name="seo_published_at" label="SEO Published At" />
         <SwitchField name="is_unlisted" label="Unlisted" />
       </CardBody>
