bddf442a0054f717f424a144736767b8f498ac8d
Greg Brown <greg@12dnetworks.com>
2024-06-18 12:17:15 -0700

    search-channels-screen updated

--- src/plugins/taxonomy/components/search-channels-screen/index.js
+++ src/plugins/taxonomy/components/search-channels-screen/index.js
@@ -1,5 +1,5 @@
 import React, { lazy } from 'react';
-import { Badge, Button, Card, Table } from 'reactstrap';
+import { Button, Card, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import SearchChannelsSort from '@triniti/schemas/triniti/taxonomy/enums/SearchChannelsSort.js';
 import { CreateModalButton, Icon, Loading, Screen } from '@triniti/cms/components/index.js';
@@ -7,16 +7,12 @@ import nodeUrl from '@triniti/cms/plugins/ncr/nodeUrl.js';
 import useRequest from '@triniti/cms/plugins/pbjx/components/useRequest.js';
 import withRequest from '@triniti/cms/plugins/pbjx/components/with-request/index.js';
 import usePolicy from '@triniti/cms/plugins/iam/components/usePolicy.js';
-import SearchForm from '@triniti/cms/plugins/taxonomy/components/search-channels-screen/SearchForm.js';
-import Collaborators from '@triniti/cms/plugins/raven/components/collaborators/index.js';
-import NodeRef from '@gdbots/pbj/well-known/NodeRef.js';
-import withForm from '@triniti/cms/components/with-form/index.js';
 
 const CreateChannelModal = lazy(() => import('@triniti/cms/plugins/taxonomy/components/create-channel-modal/index.js'));
 
 function SearchChannelsScreen(props) {
   const { request } = props;
-  const { response, run, isRunning, pbjxError } = useRequest(request, true);
+  const { response, pbjxError } = useRequest(request, true);
   const policy = usePolicy();
   const canCreate = policy.isGranted(`${APP_VENDOR}:channel:create`);
   const canUpdate = policy.isGranted(`${APP_VENDOR}:channel:update`);
@@ -27,30 +23,28 @@ function SearchChannelsScreen(props) {
       header="Channels"
       primaryActions={
         <>
-          {isRunning && <Badge color="light" pill><span className="badge-animated">Searching</span></Badge>}
-          {canCreate && <CreateModalButton text="Create Channel" modal={CreateChannelModal} />}
+          {canCreate && <CreateModalButton text="Create Channel" icon="plus-outline" modal={CreateChannelModal} />}
         </>
       }
     >
-      <SearchForm {...props} isRunning={isRunning} run={run} />
       {(!response || pbjxError) && <Loading error={pbjxError} />}
 
       {response && response.has('nodes') && (
         <Card>
-          <Table responsive>
+          <Table hover responsive>
             <tbody>
             {response.get('nodes').map(node => (
               <tr key={`${node.get('_id')}`}>
-                <td>{node.get('title')} <Collaborators nodeRef={NodeRef.fromNode(node)} /></td>
+                <td>{node.get('title')}</td>
                 <td className="td-icons">
                   <Link to={nodeUrl(node, 'view')}>
-                    <Button color="hover">
+                    <Button color="hover" tabIndex="-1">
                       <Icon imgSrc="eye" alt="view" />
                     </Button>
                   </Link>
                   {canUpdate && (
                     <Link to={nodeUrl(node, 'edit')}>
-                      <Button color="hover">
+                      <Button color="hover" tabIndex="-1">
                         <Icon imgSrc="pencil" alt="edit" />
                       </Button>
                     </Link>
@@ -71,7 +65,7 @@ function SearchChannelsScreen(props) {
   );
 }
 
-export default withRequest(withForm(SearchChannelsScreen), 'triniti:taxonomy:request:search-channels-request', {
+export default withRequest(SearchChannelsScreen, 'triniti:taxonomy:request:search-channels-request', {
   persist: true,
   initialData: {
     count: 50,
