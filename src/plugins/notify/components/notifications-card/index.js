1d758e0530d50cfb1e7844df28bdbf298012032e
Greg Brown <greg@12dnetworks.com>
2024-06-24 14:42:38 -0700

    article-screen cleanup notifications tab

--- src/plugins/notify/components/notifications-card/index.js
+++ src/plugins/notify/components/notifications-card/index.js
@@ -12,13 +12,10 @@ import nodeUrl from '@triniti/cms/plugins/ncr/nodeUrl.js';
 
 const CreateNotificationModal = lazy(() => import('@triniti/cms/plugins/notify/components/create-notification-modal/index.js'));
 
-
-function HasNotificationsCard(props) {
-  const { editMode, node, contentRef, request } = props;
+function NotificationsCard(props) {
+  const { contentRef, request } = props;
   request.set('content_ref', NodeRef.fromString(contentRef));
-
   const { response, pbjxError } = useRequest(request, true);
-
   const policy = usePolicy();
   const canCreate = policy.isGranted(`${APP_VENDOR}:notify:create`);
 
@@ -27,58 +24,61 @@ function HasNotificationsCard(props) {
       <Card>
         <CardHeader>
           Notifications
-          {canCreate && <CreateModalButton text="Create Notification" modal={CreateNotificationModal} modalProps={{contentRef: contentRef}} disabled={!editMode} />}
+          {canCreate && (
+            <CreateModalButton
+              text="Create Notification"
+              icon="plus-outline"
+              modal={CreateNotificationModal}
+              modalProps={{ contentRef: contentRef }}
+            />
+          )}
         </CardHeader>
         <CardBody>
           {(!response || pbjxError) && <Loading error={pbjxError} />}
 
           {response && (
-            <>
-              <Card>
-                <Table responsive>
-                  <thead>
-                    <tr>
-                      <th>Type</th>
-                      <th>Creation Date</th>
-                      <th>Send At</th>
-                      <th>Status</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {response.get('nodes', []).map(node => {
-                      const schema = node.schema();
-                      const canUpdate = policy.isGranted(`${schema.getQName()}:update`);
-                      return (
-                        <tr key={`${node.get('_id')}`} className={`status-${node.get('send_status')}`}>
-                          <td>
-                            {schema.getCurie().getMessage()}
-                            {node.has('apple_news_operation') ? ` (${node.get('apple_news_operation')})` : ''}
-                          </td>
-                          <td className="text-nowrap">{formatDate(node.get('created_at'))}</td>
-                          <td className="text-nowrap">{formatDate(node.get('send_at'))}</td>
-                          <td className="text-nowrap">{node.get('send_status').toString()}</td>
-                          <td className="td-icons">
-                            <Link to={nodeUrl(node, 'view')}>
-                              <Button id={`view-${node.get('_id')}`} color="hover">
-                                <Icon imgSrc="eye" alt="view" />
-                              </Button>
-                            </Link>
-                            {canUpdate && (
-                              <Link to={nodeUrl(node, 'edit')}>
-                                <Button color="hover">
-                                  <Icon imgSrc="pencil" alt="edit" />
-                                </Button>
-                              </Link>
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </Card>
-            </>
+            <Table hover responsive>
+              <thead>
+              <tr>
+                <th>Type</th>
+                <th>Created At</th>
+                <th>Send At</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+              </thead>
+              <tbody>
+              {response.get('nodes', []).map(node => {
+                const schema = node.schema();
+                const canUpdate = policy.isGranted(`${schema.getQName()}:update`);
+                return (
+                  <tr key={`${node.get('_id')}`} className={`status-${node.get('send_status')}`}>
+                    <td>
+                      {schema.getCurie().getMessage()}
+                      {node.has('apple_news_operation') ? ` (${node.get('apple_news_operation')})` : ''}
+                    </td>
+                    <td className="text-nowrap">{formatDate(node.get('created_at'))}</td>
+                    <td className="text-nowrap">{formatDate(node.get('send_at'))}</td>
+                    <td className="text-nowrap">{node.get('send_status').toString()}</td>
+                    <td className="td-icons">
+                      <Link to={nodeUrl(node, 'view')}>
+                        <Button color="hover" tabIndex="-1">
+                          <Icon imgSrc="eye" alt="view" />
+                        </Button>
+                      </Link>
+                      {canUpdate && (
+                        <Link to={nodeUrl(node, 'edit')}>
+                          <Button color="hover" tabIndex="-1">
+                            <Icon imgSrc="pencil" alt="edit" />
+                          </Button>
+                        </Link>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+              </tbody>
+            </Table>
           )}
         </CardBody>
       </Card>
@@ -86,9 +86,11 @@ function HasNotificationsCard(props) {
   );
 }
 
-export default withRequest(HasNotificationsCard, 'triniti:notify:request:search-notifications-request', {
-  channel: 'article-notifications-tab',
+export default withRequest(NotificationsCard, 'triniti:notify:request:search-notifications-request', {
+  channel: 'tab',
   initialData: {
     sort: SearchNotificationsSort.CREATED_AT_DESC.getValue(),
+    count: 50,
+    track_total_hits: false,
   }
 });
