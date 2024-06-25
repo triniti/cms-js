06145fc991577d4309cc302b1778cfbd8eeda370
Greg Brown <greg@12dnetworks.com>
2024-06-25 10:51:06 -0700

    create-widget-modal updated

--- src/plugins/curator/components/create-widget-modal/CreateWidgetForm.js
+++ src/plugins/curator/components/create-widget-modal/CreateWidgetForm.js
@@ -55,6 +55,7 @@ function CreateWidgetForm(props) {
         <ActionButton
           text="Cancel"
           onClick={props.toggle}
+          icon="close-sm"
           color="light"
           tabIndex="-1"
         />
@@ -62,6 +63,7 @@ function CreateWidgetForm(props) {
           text={`Create ${label}`}
           onClick={delegate.handleCreate}
           disabled={submitDisabled}
+          icon="plus-outline"
           color="primary"
         />
       </ModalFooter>
