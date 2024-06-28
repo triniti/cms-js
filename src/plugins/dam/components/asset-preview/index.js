7ebfda630f88da9c2892bc1e8233ddf58d5b79ee
mgollnick <mgollnick@gmail.com>
2024-06-28 13:54:55 -0700

    - Add downloadUrl link to asset preview

--- src/plugins/dam/components/asset-preview/index.js
+++ src/plugins/dam/components/asset-preview/index.js
@@ -5,9 +5,11 @@ import { Icon } from '@triniti/cms/components/index.js';
 
 function ArchiveAsset({ asset, downloadUrl }) {
   return (
-    <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
-      <Icon imgSrc="zip" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
-    </div>
+    <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="hover-box-shadow d-block rounded-3">
+      <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
+        <Icon imgSrc="zip" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
+      </div>
+    </a>
   );
 }
 
@@ -19,24 +21,28 @@ function AudioAsset({ asset, downloadUrl }) {
 
 function CodeAsset({ asset, downloadUrl }) {
   return (
-    <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
-      <Icon imgSrc="code" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
-    </div>
+    <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="hover-box-shadow d-block rounded-3">
+      <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
+        <Icon imgSrc="code" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
+      </div>
+    </a>
   );
 }
 
 function DocumentAsset({ asset, downloadUrl }) {
   return (
-    <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
-      <Icon imgSrc="document" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
-    </div>
+    <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="hover-box-shadow d-block rounded-3">
+      <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
+        <Icon imgSrc="document" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
+      </div>
+    </a>
   );
 }
 
 function ImageAsset({ asset, downloadUrl }) {
   const previewUrl = damUrl(asset, '1by1', 'md');
   return (
-    <a href={downloadUrl} target="_blank" rel="noopener noreferrer">
+    <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="hover-box-shadow d-block rounded-3">
       <Media src={previewUrl} alt="" width="100%" height="auto" object className="rounded-3" />
     </a>
   );
@@ -44,9 +50,11 @@ function ImageAsset({ asset, downloadUrl }) {
 
 function UnknownAsset({ asset, downloadUrl }) {
   return (
-    <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
-      <Icon imgSrc="question-outline" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
-    </div>
+    <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="hover-box-shadow d-block rounded-3">
+      <div className="ratio ratio-16x9 bg-body-secondary rounded-3 mb-3">
+        <Icon imgSrc="question-outline" alt="" className="position-absolute top-50 start-50 translate-middle" color="dark" size="xxl" />
+      </div>
+    </a>
   );
 }
 
