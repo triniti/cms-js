0d53de815995a13e085d25bbfb85235b2dc31e8e
Greg Brown <greg@12dnetworks.com>
2024-07-08 14:01:27 -0700

    stubby

--- src/blocksmith/index.js
+++ src/blocksmith/index.js
@@ -1,5 +1,76 @@
 import React from 'react';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
+import ToolbarPlugin from '@triniti/cms/blocksmith/plugins/ToolbarPlugin.js';
+
+const theme = {
+  code: 'editor-code',
+  heading: {
+    h1: 'editor-heading-h1',
+    h2: 'editor-heading-h2',
+    h3: 'editor-heading-h3',
+    h4: 'editor-heading-h4',
+    h5: 'editor-heading-h5',
+  },
+  image: 'editor-image',
+  link: 'editor-link',
+  list: {
+    listitem: 'editor-listitem',
+    nested: {
+      listitem: 'editor-nested-listitem',
+    },
+    ol: 'editor-list-ol',
+    ul: 'editor-list-ul',
+  },
+  ltr: 'ltr',
+  paragraph: 'editor-paragraph',
+  placeholder: 'editor-placeholder',
+  quote: 'editor-quote',
+  rtl: 'rtl',
+  text: {
+    bold: 'editor-text-bold',
+    code: 'editor-text-code',
+    hashtag: 'editor-text-hashtag',
+    italic: 'editor-text-italic',
+    overflowed: 'editor-text-overflowed',
+    strikethrough: 'editor-text-strikethrough',
+    underline: 'editor-text-underline',
+    underlineStrikethrough: 'editor-text-underlineStrikethrough',
+  },
+};
+
+const config = {
+  namespace: 'blocksmith',
+  nodes: [],
+  onError(error) {
+    console.error(error);
+  },
+  theme,
+};
 
 export default function Blocksmith(props) {
-  return <textarea value={JSON.stringify(props.node.toObject())} />;
+
+  return (
+    <LexicalComposer initialConfig={config}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder="enter stuff"
+                placeholder={
+                  <div className="editor-placeholder">enter stuff</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
 }
