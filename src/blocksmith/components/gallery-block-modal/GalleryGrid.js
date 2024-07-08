695368570f0c849e3de243c6cbc3bb536db916f7
Greg Brown <greg@12dnetworks.com>
2024-07-08 09:35:53 -0700

    remove code we refactored

--- src/blocksmith/components/gallery-block-modal/GalleryGrid.js
+++ src/blocksmith/components/gallery-block-modal/GalleryGrid.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Loading, withForm } from '@triniti/cms/components/index.js';
 import useRequest from '@triniti/cms/plugins/pbjx/components/useRequest.js';
 import withRequest from '@triniti/cms/plugins/pbjx/components/with-request/index.js';
-import ImageGrid from '@triniti/cms/plugins/dam/components/refactor-wip/image-grid-old/index.js';
 import SearchForm from '@triniti/cms/plugins/dam/components/asset-picker-field/SearchForm.js';
+
+const ImageGrid = () => null;
 
 const GalleryGrid = (props) => {
   const {
--- src/plugins/dam/components/refactor-wip/batch-edit-modal/index.js
+++
@@ -1,3 +0,0 @@
-import { lazy } from 'react'
-
-export default lazy(() => import('@triniti/cms/plugins/dam/components/batch-edit-modal/BatchEdit.js'));
