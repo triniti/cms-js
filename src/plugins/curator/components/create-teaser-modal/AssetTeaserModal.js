a3d5b328a236b8eef950ed819ec805a41fe8eb5a
Greg Brown <greg@12dnetworks.com>
2024-07-08 09:52:55 -0700

    put form errors inot modal body

--- src/plugins/curator/components/create-teaser-modal/AssetTeaserModal.js
+++ src/plugins/curator/components/create-teaser-modal/AssetTeaserModal.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { ModalBody } from 'reactstrap';
 import ImageAssetPickerField from '@triniti/cms/plugins/dam/components/image-asset-picker-field/index.js';
 import withTeaserModal from '@triniti/cms/plugins/curator/components/create-teaser-modal/withTeaserModal.js';
 
 // todo: review, should this be limited to images?  seems odd.
 function AssetTeaserModal() {
   return (
-    <ModalBody>
+    <>
       <ImageAssetPickerField name="target_ref" label="Target Asset" required />
-    </ModalBody>
+    </>
   );
 }
 
--- src/plugins/curator/components/create-teaser-modal/ChannelTeaserModal.js
+++ src/plugins/curator/components/create-teaser-modal/ChannelTeaserModal.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { ModalBody } from 'reactstrap';
 import ChannelPickerField from '@triniti/cms/plugins/taxonomy/components/channel-picker-field/index.js';
 import withTeaserModal from '@triniti/cms/plugins/curator/components/create-teaser-modal/withTeaserModal.js';
 
 function ChannelTeaserModal() {
   return (
-    <ModalBody>
+    <>
       <ChannelPickerField name="target_ref" label="Target Channel" required />
-    </ModalBody>
+    </>
   );
 }
 
