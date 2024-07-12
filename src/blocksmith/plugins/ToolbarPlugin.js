8be20bb60fa641f127c127709f81426a1031810b
Greg Brown <greg@12dnetworks.com>
2024-07-12 16:26:41 -0700

    blocksmith config and toolbar re-org

--- src/blocksmith/plugins/ToolbarPlugin.js
+++ src/blocksmith/plugins/ToolbarPlugin.js
@@ -75,6 +75,10 @@ export default function ToolbarPlugin() {
     };
   };
 
+  if (!editor.isEditable()) {
+    return null;
+  }
+
   return (
     <>
       <div className="toolbar sticky-top" ref={toolbarRef}>
