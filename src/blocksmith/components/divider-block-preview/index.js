1c7f9da301110886323140ed3dd1cfe1d6766d61
Greg Brown <greg@12dnetworks.com>
2024-07-17 22:52:10 -0700

    example site level styling of block

--- src/blocksmith/components/divider-block-preview/index.js
+++ src/blocksmith/components/divider-block-preview/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
 function DividerBlockPreview(props) {
-  const { pbj } = props;
+  const { block } = props;
+  const color = block.get('stroke_color', 'primary');
+  const style = block.get('stroke_style', 'solid');
   return (
-    <p>{JSON.stringify(pbj)}</p>
+    <div className={`block-stroke-${color}-${style}`}>
+      <span className="block-text">{block.get('text')}</span>
+      <hr />
+    </div>
   );
 }
 
