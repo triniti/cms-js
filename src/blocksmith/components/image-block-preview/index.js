24435d7f5f35a74500522852bb086023d1659d41
Greg Brown <greg@12dnetworks.com>
2024-07-17 21:22:54 -0700

    sample noodles

--- src/blocksmith/components/image-block-preview/index.js
+++ src/blocksmith/components/image-block-preview/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Media } from 'reactstrap';
+import { Col, Media, Row, Table } from 'reactstrap';
 import damUrl from '@triniti/cms/plugins/dam/damUrl.js';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
@@ -10,19 +10,40 @@ function ImageBlockPreview(props) {
   const imageUrl = damUrl(imageAsset.get('_id'), version, 'sm');
 
   return (
-    <>
-      <Media
-        className={`block-image rounded-3 ratioxx xxratio-${version.replace('by', 'x')}`}
-        src={imageUrl}
-        alt=""
-        width="100"
-        height="auto"
-        object
-      />
-      <p className="block-title">{block.get('title', imageAsset.get('title'), '')}</p>
-      {block.has('caption') && <p className="block-caption">{block.get('caption')}</p>}
-      {block.has('launch_text') && <p className="block-launch_text">{block.get('launch_text')}</p>}
-    </>
+    <Row>
+      <Col sm={4} xl={4}>
+        <Media
+          className={`block-image rounded-3 ratioxx xxratio-${version.replace('by', 'x')}`}
+          src={imageUrl}
+          alt=""
+          width="100"
+          height="auto"
+          object
+        />
+      </Col>
+      <Col sm={8} xl={8}>
+        <Table className="border-bottom mt-2" size="sm">
+          <tbody>
+          <tr>
+            <th className="nowrap ps-2" scope="row">Title:</th>
+            <td className="w-100 text-break">{block.get('title', imageAsset.get('title'), '')}</td>
+          </tr>
+          {block.has('caption') && (
+            <tr>
+              <th className="nowrap ps-2" scope="row">Caption:</th>
+              <td className="w-100 text-break">{block.get('caption')}</td>
+            </tr>
+          )}
+          {block.has('launch_text') && (
+            <tr>
+              <th className="nowrap ps-2" scope="row">Launch Text:</th>
+              <td className="w-100 text-break">{block.get('launch_text')}</td>
+            </tr>
+          )}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
   );
 }
 
