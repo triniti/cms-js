475488e2cc07abc36b5ec3212294aceb087b9d68
Greg Brown <greg@12dnetworks.com>
2024-07-18 14:58:53 -0700

    youtube

--- src/blocksmith/components/youtube-video-block-preview/index.js
+++ src/blocksmith/components/youtube-video-block-preview/index.js
@@ -3,20 +3,19 @@ import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-prev
 import damUrl from '@triniti/cms/plugins/dam/damUrl.js';
 
 function YoutubeVideoBlockPreview(props) {
-  const { pbj } = props;
-
-  let url = `https://www.youtube.com/watch?v=${pbj.get('id')}`;
-  if (pbj.get('start_at') > 0) {
-    url += `&t=${pbj.get('start_at')}`;
+  const { block } = props;
+  let url = `https://www.youtube.com/watch?v=${block.get('id')}`;
+  if (block.get('start_at') > 0) {
+    url += `&t=${block.get('start_at')}`;
   }
 
   return (
     <p>
       <a href={url} target="_blank" rel="noopener noreferrer">
-        {pbj.has('poster_image_ref') && (
-          <img src={damUrl(pbj.get('poster_image_ref'), '16by9', 'sm')} width={200} height={200} />
+        {block.has('poster_image_ref') && (
+          <img src={damUrl(block.get('poster_image_ref'), '16by9', 'sm')} width={200} height={200} />
         )}
-        {!pbj.has('poster_image_ref') && (
+        {!block.has('poster_image_ref') && (
           <>{url}</>
         )}
       </a>
