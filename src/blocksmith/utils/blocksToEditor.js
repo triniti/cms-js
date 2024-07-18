c3a4ce7b881fc1e9a1b41beb72d0c76df44924c8
Greg Brown <greg@12dnetworks.com>
2024-07-17 21:57:48 -0700

    nits

--- src/blocksmith/utils/blocksToEditor.js
+++ src/blocksmith/utils/blocksToEditor.js
@@ -1,4 +1,4 @@
-import { $createParagraphNode, $getRoot, $insertNodes } from 'lexical';
+import { $getRoot, $insertNodes } from 'lexical';
 import { $generateNodesFromDOM } from '@lexical/html';
 import { $createBlocksmithNode } from '@triniti/cms/blocksmith/nodes/BlocksmithNode.js';
 import { BLOCKSMITH_HYDRATION } from '@triniti/cms/blocksmith/plugins/BlocksmithPlugin.js';
@@ -7,25 +7,17 @@ export default (blocks, editor) => {
   editor.update(() => {
     const parser = new DOMParser();
     const $nodes = [];
-    let addParagraph = false;
+
     for (const block of blocks) {
       const curie = block.schema().getCurie();
       if (curie.getMessage() === 'text-block') {
         if (block.has('text')) {
           const dom = parser.parseFromString(block.get('text'), 'text/html');
           $nodes.push(...$generateNodesFromDOM(editor, dom));
-          addParagraph = false;
         }
       } else {
         $nodes.push($createBlocksmithNode(curie.toString(), block.toObject()));
-        addParagraph = true;
       }
-    }
-
-    // better user experience to have this at the end when
-    // the last node was blocksmith block.
-    if (addParagraph && editor.isEditable()) {
-      $nodes.push($createParagraphNode());
     }
 
     $getRoot().clear().select();
