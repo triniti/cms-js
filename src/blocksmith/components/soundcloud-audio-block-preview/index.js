aa4d0f985b8d83f4191a7c560b9a10673a6065a9
Greg Brown <greg@12dnetworks.com>
2024-07-18 13:45:50 -0700

    soundcloud block done, nuff for now

--- src/blocksmith/components/soundcloud-audio-block-preview/index.js
+++ src/blocksmith/components/soundcloud-audio-block-preview/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
 function SoundcloudAudioBlockPreview(props) {
-  const { pbj } = props;
+  const { block } = props;
+  let url = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${block.get('track_id')}`;
+  url += `&auto_play=${block.get('auto_play') ? 'true' : 'false'}`;
+  url += `&hide_related=${block.get('hide_related') ? 'true' : 'false'}`;
+  url += `&show_comments=${block.get('show_comments') ? 'true' : 'false'}`;
+  url += `&visual=${block.get('visual') ? 'true' : 'false'}`;
+
   return (
-    <p>{JSON.stringify(pbj)}</p>
+    <p>
+      <a href={url} target="_blank" rel="noreferrer noopener">{url}</a>
+    </p>
   );
 }
 
