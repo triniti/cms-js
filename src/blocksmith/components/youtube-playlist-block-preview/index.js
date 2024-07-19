54658a8c0e98bdb030245d9af846a82a8e73a424
Greg Brown <greg@12dnetworks.com>
2024-07-18 21:22:40 -0700

    youtube playlist block

--- src/blocksmith/components/youtube-playlist-block-preview/index.js
+++ src/blocksmith/components/youtube-playlist-block-preview/index.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
+import { Col, Media, Row, Table } from 'reactstrap';
 import damUrl from '@triniti/cms/plugins/dam/damUrl.js';
+import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
 function YoutubePlaylistBlockPreview(props) {
   const { block } = props;
-  let url = `https://www.youtube.com/watch?list=${block.get('playlist_id')}`;
-  if (block.has('video_id')) {
-    url += `&v=${block.get('video_id')}`;
+
+  if (block.has('poster_image_ref')) {
+    let url = `https://www.youtube.com/watch?list=${block.get('playlist_id')}`;
+    if (block.has('video_id')) {
+      url += `&v=${block.get('video_id')}`;
+    }
+
+    return (
+      <Row className="gx-2">
+        <Col xs={2}>
+          <a href={url} className="hover-box-shadow d-inline-block rounded-2" target="_blank" rel="noreferrer noopener">
+            <Media
+              src={damUrl(block.get('poster_image_ref'), '16by9', 'sm')}
+              className="rounded-2 ratio-16x9"
+              alt=""
+              width="100%"
+              height="auto"
+              object
+            />
+          </a>
+        </Col>
+        <Col>
+          <Table borderless size="sm">
+            <tbody>
+            <tr>
+              <th className="nowrap ps-2 pt-0" scope="row">URL:</th>
+              <td className="w-100 text-break pt-0">
+                <a href={url} target="_blank" rel="noreferrer noopener">{url}</a>
+              </td>
+            </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    );
   }
 
+  let url = 'https://www.youtube-nocookie.com/embed/';
+  if (block.has('video_id')) {
+    url += block.get('video_id');
+  }
+
+  url += `?list=${block.get('playlist_id')}`;
+
   return (
-    <p>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        {block.has('poster_image_ref') && (
-          <img src={damUrl(block.get('poster_image_ref'), '16by9', 'sm')} width={200} height={200} />
-        )}
-        {!block.has('poster_image_ref') && (
-          <>{url}</>
-        )}
-      </a>
-    </p>
+    <div className="embed-responsive embed-responsive-16by9 text-center">
+      <iframe width="560" height="315" src={url}></iframe>
+    </div>
   );
 }
 
