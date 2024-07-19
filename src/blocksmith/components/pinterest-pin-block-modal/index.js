4a18dfcfdb31515b01f96f1ca3b8579f0a094e22
Greg Brown <greg@12dnetworks.com>
2024-07-18 17:53:57 -0700

    nits, imgur ids are the data-id value from the embed code, not the slug in urls (those vary by type and have changed over time)

--- src/blocksmith/components/pinterest-pin-block-modal/index.js
+++ src/blocksmith/components/pinterest-pin-block-modal/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {SelectField, SwitchField, UrlField} from '@triniti/cms/components/index.js';
+import { SelectField, SwitchField, UrlField } from '@triniti/cms/components/index.js';
 import withBlockModal from '@triniti/cms/blocksmith/components/with-block-modal/index.js';
 
-const types = [
-    { label: 'small', value: 'small' },
-    { label: 'medium', value: 'medium' },
-    { label: 'large', value: 'large' },
+const sizes = [
+  { label: 'small', value: 'small' },
+  { label: 'medium', value: 'medium' },
+  { label: 'large', value: 'large' },
 ];
 
 function PinterestPinBlockModal() {
@@ -13,7 +13,7 @@ function PinterestPinBlockModal() {
     <>
       <UrlField name="href" label="Pinterest Pin URL" required />
       <SwitchField name="terse" label="Hide Description" />
-      <SelectField name="size" label="Size" options={types} />
+      <SelectField name="size" label="Size" options={sizes} />
     </>
   );
 }
