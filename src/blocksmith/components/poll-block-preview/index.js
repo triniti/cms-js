01c761315f7b8db8ad7800e47a22a5bfde41fe2f
Greg Brown <greg@12dnetworks.com>
2024-07-18 20:35:05 -0700

    poll block

--- src/blocksmith/components/poll-block-preview/index.js
+++ src/blocksmith/components/poll-block-preview/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Badge, Button, Col, Row, Table } from 'reactstrap';
+import { Badge, Button, Col, Media, Row, Table } from 'reactstrap';
+import damUrl from '@triniti/cms/plugins/dam/damUrl.js';
 import nodeUrl from '@triniti/cms/plugins/ncr/nodeUrl.js';
 import { Icon } from '@triniti/cms/components/index.js';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
@@ -7,10 +8,18 @@ import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-prev
 function PollBlockPreview(props) {
   const { block, node } = props;
   const status = node.get('status').getValue();
+  const imageUrl = damUrl(node.get('image_ref'), '1by1', 'sm');
   const url = nodeUrl(node, 'view');
 
   return (
     <Row className="gx-2">
+      {imageUrl && (
+        <Col xs={2}>
+          <a href={url} className="hover-box-shadow d-inline-block rounded-2" target="_blank">
+            <Media src={imageUrl} className="rounded-2 ratio-1x1" alt="" width="100%" height="auto" object />
+          </a>
+        </Col>
+      )}
       <Col>
         <Table borderless size="sm">
           <tbody>
