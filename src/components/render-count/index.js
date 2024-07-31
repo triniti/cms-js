e19ee285f1a722ac7a794d9f7bd563e7b8800dac
Greg Brown <greg@12dnetworks.com>
2024-07-30 17:00:16 -0700

    remove some unused components

--- src/components/render-count/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useRef } from 'react';
-
-export default function RenderCount() {
-  const renders = useRef(0);
-  return <label>count={++renders.current}</label>;
-}
--- src/components/scrollable-container/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classNames from 'classnames';
-import '@triniti/cms/components/scrollable-container/styles.scss';
-
-export default function ScrollableContainer({ children, className, tag: Tag = 'span', ...attributes }) {
-  const classes = classNames(className, 'scrollable-container');
-  return <Tag {...attributes} className={classes}>{children}</Tag>;
-}
