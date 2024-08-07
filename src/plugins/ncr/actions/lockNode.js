97c55d66cd02fe679864dbb875636cb519cfd5a1
Greg Brown <greg@12dnetworks.com>
2024-08-07 16:47:37 -0700

    nits

--- src/plugins/ncr/actions/lockNode.js
+++ src/plugins/ncr/actions/lockNode.js
@@ -1,9 +1,9 @@
 import MessageResolver from '@gdbots/pbj/MessageResolver.js';
 import NodeRef from '@gdbots/pbj/well-known/NodeRef.js';
 
-export default nodeRef => async (dispatch, getState, app) => {
-  const LockedNodeV1 = await MessageResolver.resolveCurie('gdbots:ncr:command:lock-node:v1');
+export default (nodeRef) => async (dispatch, getState, app) => {
+  const LockNodeV1 = await MessageResolver.resolveCurie('gdbots:ncr:command:lock-node:v1');
   const pbjx = app.getPbjx();
-  const command = LockedNodeV1.create().set('node_ref', NodeRef.fromString(`${nodeRef}`));
+  const command = LockNodeV1.create().set('node_ref', NodeRef.fromString(`${nodeRef}`));
   await pbjx.send(command);
 };
--- src/plugins/ncr/actions/unlockNode.js
+++ src/plugins/ncr/actions/unlockNode.js
@@ -1,9 +1,9 @@
 import MessageResolver from '@gdbots/pbj/MessageResolver.js';
 import NodeRef from '@gdbots/pbj/well-known/NodeRef.js';
 
-export default nodeRef => async (dispatch, getState, app) => {
-  const UnlockedNodeV1 = await MessageResolver.resolveCurie('gdbots:ncr:command:unlock-node:v1');
+export default (nodeRef) => async (dispatch, getState, app) => {
+  const UnlockNodeV1 = await MessageResolver.resolveCurie('gdbots:ncr:command:unlock-node:v1');
   const pbjx = app.getPbjx();
-  const command = UnlockedNodeV1.create().set('node_ref', NodeRef.fromString(`${nodeRef}`));
+  const command = UnlockNodeV1.create().set('node_ref', NodeRef.fromString(`${nodeRef}`));
   await pbjx.send(command);
 };
