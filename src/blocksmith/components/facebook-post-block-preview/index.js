322db9e448e44e5e2d511553f70de4248bce858b
rsumilang <rsumilang@users.noreply.github.com>
2025-01-10 14:17:13 -0800

    wip

--- src/blocksmith/components/facebook-post-block-preview/index.js
+++ src/blocksmith/components/facebook-post-block-preview/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Col, Row, Table } from 'reactstrap';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
 function FacebookPostBlockPreview(props) {
   const { block } = props;
   return (
-    <p>
-      <a href={block.get('href')} target="_blank" rel="noreferrer noopener">{block.get('href')}</a>
-    </p>
+    <Row className='gx-2'>
+      <Col xs='5'>
+        <iframe src={`/block-previewer.html?type=fbpost&width=350&href=${encodeURIComponent(block.get('href'))}&show_text=${block.get('show_text')}`} width={"100%"} sandbox></iframe>
+      </Col>
+      <Col>
+        <Table borderless size="sm">
+          <tbody>
+          <tr>
+            <th className="nowrap ps-2 pt-0" scope="row">Post Url:</th>
+            <td className="w-100 text-break pt-0">
+              <a href={block.get('href')} target="_blank" rel="noreferrer noopener">{block.get('href')}</a>
+            </td>
+          </tr>
+          <tr>
+            <th className="nowrap ps-2" scope="row">Show Text:</th>
+            <td className="w-100 text-break">{block.get('show_text') ? 'Yes' : 'No'}</td>
+          </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
   );
 }
 
--- src/blocksmith/components/instagram-media-block-preview/index.js
+++ src/blocksmith/components/instagram-media-block-preview/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Col, Row, Table } from 'reactstrap';
 import withBlockPreview from '@triniti/cms/blocksmith/components/with-block-preview/index.js';
 
 function InstagramMediaBlockPreview(props) {
   const { block } = props;
   const url = `https://www.instagram.com/p/${block.get('id')}/`;
   return (
-    <p>
-      <a href={url} target="_blank" rel="noreferrer noopener">{url}</a>
-    </p>
+        <Row className='gx-2'>
+          <Col xs='5'>
+            <iframe src={`/block-previewer.html?type=igmedia&width=350&url=${encodeURIComponent(url)}&hidecaption=${block.get('hidecaption')}`} width={"100%"} sandbox></iframe>
+          </Col>
+          <Col>
+            <Table borderless size="sm">
+              <tbody>
+              <tr>
+                <th className="nowrap ps-2 pt-0" scope="row">Media Url:</th>
+                <td className="w-100 text-break pt-0">
+                  <a href={url} target="_blank" rel="noreferrer noopener">{url}</a>
+                </td>
+              </tr>
+              <tr>
+                <th className="nowrap ps-2" scope="row">Hide Caption:</th>
+                <td className="w-100 text-break">{block.get('hidecaption') ? 'Yes' : 'No'}</td>
+              </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
   );
 }
 
