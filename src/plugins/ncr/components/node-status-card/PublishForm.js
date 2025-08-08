f381551e789b720ba4b4ccac19efa36e17dde9a2
guizzettic <guizzettic@gmail.com>
2025-08-08 11:53:34 -0700

    remove uneccessary null change

--- src/plugins/ncr/components/node-status-card/PublishForm.js
+++ src/plugins/ncr/components/node-status-card/PublishForm.js
@@ -111,7 +111,7 @@ export default function PublishForm(props) {
   const handleSelect = (evt) => {
     const newAction = kebabCase(evt.target.innerText);
     if (newAction === 'publish') {
-      setPublishAt(null);
+      setPublishAt(new Date());
     } else {
       setPublishAt(node.get('published_at') || new Date());
     }
