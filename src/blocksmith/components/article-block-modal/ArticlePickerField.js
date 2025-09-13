1ffb1bc7186f0f310e596a054f78730581a1b377
meghedi <hacopian.m@gmail.com>
2025-09-12 17:07:31 -0700

    remove vendor custom example code

--- src/blocksmith/components/article-block-modal/ArticlePickerField.js
+++ src/blocksmith/components/article-block-modal/ArticlePickerField.js
@@ -3,15 +3,12 @@
  */
 import React from 'react';
 import ArticlePickerField from '@triniti/cms/plugins/news/components/article-picker-field/index.js';
-import NodeStatus from "@gdbots/schemas/gdbots/ncr/enums/NodeStatus.js";
 
 const ArticleBlockArticlePickerField = (props) => {
-    const defaultStatuses = [NodeStatus.SCHEDULED, NodeStatus.PUBLISHED];
-
     return (
         <ArticlePickerField
             {...props}
-            statuses={props.statuses || defaultStatuses}
+            statuses={props.statuses}
         />
     );
 };
--- src/blocksmith/components/gallery-block-modal/GalleryPickerField.js
+++ src/blocksmith/components/gallery-block-modal/GalleryPickerField.js
@@ -3,15 +3,12 @@
  */
 import React from 'react';
 import GalleryPickerField from '@triniti/cms/plugins/curator/components/gallery-picker-field/index.js';
-import NodeStatus from "@gdbots/schemas/gdbots/ncr/enums/NodeStatus.js";
 
 const GalleryBlockGalleryPickerField = (props) => {
-    const defaultStatuses = [NodeStatus.PUBLISHED];
-
     return (
         <GalleryPickerField
             {...props}
-            statuses={props.statuses || defaultStatuses}
+            statuses={props.statuses}
         />
     );
 };
