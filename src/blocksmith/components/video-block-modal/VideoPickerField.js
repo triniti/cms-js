df7955ed130b0af36eff33d0ad07cd5131eb7edf
meghedi <hacopian.m@gmail.com>
2025-09-14 23:11:16 -0700

    make it simple

--- src/blocksmith/components/video-block-modal/VideoPickerField.js
+++ src/blocksmith/components/video-block-modal/VideoPickerField.js
@@ -1,16 +1,6 @@
 /**
  * Video Picker Field for Video Block Modal
  */
-import React from 'react';
 import VideoPickerField from '@triniti/cms/plugins/ovp/components/video-picker-field/index.js';
 
-const VideoBlockVideoPickerField = (props) => {
-    return (
-        <VideoPickerField
-            {...props}
-            statuses={props.statuses}
-        />
-    );
-};
-
-export default VideoBlockVideoPickerField;
+export default VideoPickerField;
