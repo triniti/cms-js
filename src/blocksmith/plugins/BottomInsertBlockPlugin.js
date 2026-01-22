1ef6969461ac6ec537f358a1644409e9c247fdbd
Eric Jacobson <contact@efjacobson.com>
2026-01-22 10:43:11 -0800

    insert block at bottom

--- src/blocksmith/plugins/BottomInsertBlockPlugin.js
+++ src/blocksmith/plugins/BottomInsertBlockPlugin.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'reactstrap';
 import { Icon } from '@triniti/cms/components/index.js';
-import { INSERT_BLOCK_COMMAND } from '@triniti/cms/blocksmith/plugins/BlocksmithPlugin.js';
+import { INSERT_BLOCK_AT_BOTTOM_COMMAND } from '@triniti/cms/blocksmith/plugins/BlocksmithPlugin.js';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 
 export default function BottomInsertBlockPlugin () {
@@ -9,7 +9,7 @@ export default function BottomInsertBlockPlugin () {
 
   return (
     <div className="bottom-insert-block-button">
-      <Button color="primary" className="rounded-pill" size="sm" onClick={() => editor.dispatchCommand(INSERT_BLOCK_COMMAND, {})}>
+      <Button color="primary" className="rounded-pill" size="sm" onClick={() => editor.dispatchCommand(INSERT_BLOCK_AT_BOTTOM_COMMAND, {})}>
         <Icon imgSrc="plus" alt="Insert Block" size="xs" className="me-1" />Text
       </Button>
     </div>
