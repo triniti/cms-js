a2d05a2ebf710c151df4f6c3ac7fbaea6c1e0989
Chris Guizzetti <guizzettic@gmail.com>
2026-02-04 12:33:01 -0800

    120 vertical video (#533)
    * Display `is_vertical` field on video asset details tab and video mezzanine card.

--- CHANGELOG-1.x.md
+++ CHANGELOG-1.x.md
@@ -52,6 +52,7 @@
 * Restore Persistent "Insert Block" Button Visibility
 * Blocksmith :: adjust drag image position when dragging blocks in the editor
 * asset linker modal should default to image assets only
+* Display `is_vertical` field on video asset details tab and video mezzanine card.
 
 
 ## v1.2.2
--- src/plugins/ovp/components/video-screen/MezzanineCard.js
+++ src/plugins/ovp/components/video-screen/MezzanineCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, CardHeader } from 'reactstrap';
+import { Card, CardBody, CardHeader, Table } from 'reactstrap';
 import { UrlField } from '@triniti/cms/components/index.js';
 import useNode from '@triniti/cms/plugins/ncr/components/useNode.js';
 import TranscodeableCard from '@triniti/cms/plugins/dam/components/asset-screen/TranscodeableCard.js';
@@ -16,6 +16,14 @@ export default function MezzanineCard(props) {
         <CardBody>
           <VideoAssetPickerField name="mezzanine_ref" label="Mezzanine Asset"/>
           <UrlField name="mezzanine_url" label="Mezzanine URL"/>
+          <Table className="border-bottom mb-0 mt-3" size="sm">
+            <tbody>
+              <tr>
+                <th className="nowrap ps-2" scope="row">Vertical:</th>
+                <td className="w-100">{node.get('is_vertical') ? 'Yes' : 'No'}</td>
+              </tr>
+            </tbody>
+          </Table>
         </CardBody>
       </Card>
 
