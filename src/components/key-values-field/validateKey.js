063d180e405fb96268fe0e3be056fbd081715f10
Trent Teebken <trent.teebken@fox.com>
2026-04-14 14:53:27 -0600

    Change so relaxValidation prop can be added

--- src/components/key-values-field/validateKey.js
+++ src/components/key-values-field/validateKey.js
@@ -1,18 +1,15 @@
-import AllowedMutantKeys from './AllowedMutantKeys.js';
-
 export const VALID_KEY_PATTERN = /^[a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,126}$/;
 
-export default (value, allValues, pbjName) => {
+export default (value, keyValue, relaxValidation = false) => {
   if (value === undefined || value === null || !`${value}`.length) {
     return 'Required';
   }
 
-  const isMutantKeysAllowed = AllowedMutantKeys(allValues);
-  if (!isMutantKeysAllowed[pbjName] && !VALID_KEY_PATTERN.test(value)) {
+  if (!relaxValidation && !VALID_KEY_PATTERN.test(value)) {
     return 'Only use letters, numbers and underscores.';
   }
 
-  const used = (allValues[pbjName] || []).map(o => o && o.key).filter(v => v === value);
+  const used = (keyValue || []).map(o => o && o.key).filter(v => v === value);
   if (used.length > 1) {
     return 'Duplicate Key';
   }
