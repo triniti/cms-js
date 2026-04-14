0f261d492d82439dcae89e5cb1bee83fa5d30df9
Trent Teebken <trent.teebken@fox.com>
2026-04-14 15:13:57 -0600

    make variable plural

--- src/components/key-values-field/validateKey.js
+++ src/components/key-values-field/validateKey.js
@@ -1,6 +1,6 @@
 export const VALID_KEY_PATTERN = /^[a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,126}$/;
 
-export default (value, keyValue, relaxValidation = false) => {
+export default (value, keyValues, relaxValidation = false) => {
   if (value === undefined || value === null || !`${value}`.length) {
     return 'Required';
   }
@@ -9,7 +9,7 @@ export default (value, keyValue, relaxValidation = false) => {
     return 'Only use letters, numbers and underscores.';
   }
 
-  const used = (keyValue || []).map(o => o && o.key).filter(v => v === value);
+  const used = (keyValues || []).map(o => o && o.key).filter(v => v === value);
   if (used.length > 1) {
     return 'Duplicate Key';
   }
