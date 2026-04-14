c1b75c4434ed74a02b55917805c8e22b03d5e928
guizzettic <guizzettic@gmail.com>
2026-04-14 15:42:24 -0700

    add launchText as preview prop

--- src/blocksmith/components/gallery-block-modal/index.js
+++ src/blocksmith/components/gallery-block-modal/index.js
@@ -4,12 +4,16 @@ import { SwitchField, TextField } from '@triniti/cms/components/index.js';
 import AspectRatioField from '@triniti/cms/plugins/common/components/aspect-ratio-field/index.js';
 import GalleryPickerField from '@triniti/cms/blocksmith/components/gallery-block-modal/GalleryPickerField.js';
 import ImageAssetPickerField from '@triniti/cms/plugins/dam/components/image-asset-picker-field/index.js';
+import useNode from '@triniti/cms/plugins/ncr/components/useNode.js';
 import withBlockModal from '@triniti/cms/blocksmith/components/with-block-modal/index.js';
 import AsideField from '@triniti/cms/blocksmith/components/with-block-modal/AsideField.js';
 
 function GalleryBlockModal(props) {
   const { nodeRef: containerRef } = props.containerFormContext;
   const { values } = useFormState({ subscription: { values: true } });
+  const { node: gallery } = useNode(values.node_ref || null);
+  const launchText = values.launch_text || gallery?.get('launch_text') || '';
+
   return (
     <>
       <GalleryPickerField name="node_ref" label="Gallery" required />
@@ -19,6 +23,7 @@ function GalleryBlockModal(props) {
         description="When not set, the gallery's image will be used."
         nodeRef={containerRef}
         galleryRef={values.node_ref}
+        previewProps={{ launchText }}
       />
       <SwitchField
         name="start_at_poster"
