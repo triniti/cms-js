b9c9fe82281cb23093f46d95400ca765618f3acc
rsumilang <rsumilang@users.noreply.github.com>
2026-05-11 19:28:14 -0700

    Update isInEditMode.js

--- src/plugins/iam/utils/isInEditMode.js
+++ src/plugins/iam/utils/isInEditMode.js
@@ -1,4 +1,4 @@
-import { matchPath } from 'react-router-dom';
+import { matchPath } from 'react-router';
 
 const EDIT_PATHS = [
   { path: '/ncr/:label/:id/:tab/edit', end: true, caseSensitive: false },
