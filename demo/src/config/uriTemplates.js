e48cd78a1305c2be401dfde7afd6a2b0834af5f8
Eric Jacobson <contact@efjacobson.com>
2026-05-27 12:22:11 -0700

    implement canonical

--- demo/src/config/uriTemplates.js
+++ demo/src/config/uriTemplates.js
@@ -62,6 +62,7 @@ registerTemplates({
   [`${APP_VENDOR}:channel-teaser.canonical`]: '{+web_base_url}t/channel/{_id}/',
   [`${APP_VENDOR}:gallery.canonical`]: '{+web_base_url}photos/{+slug}/',
   [`${APP_VENDOR}:gallery-teaser.canonical`]: '{+web_base_url}t/gallery/{_id}/',
+  [`${APP_VENDOR}:live-blog-update-teaser.canonical`]: '{+web_base_url}t/live-blog-update/{_id}/',
   [`${APP_VENDOR}:link-teaser.canonical`]: '{+link_url}',
   [`${APP_VENDOR}:page.canonical`]: '{+web_base_url}pages/{slug}/',
   [`${APP_VENDOR}:page-teaser.canonical`]: '{+web_base_url}t/page/{_id}/',
